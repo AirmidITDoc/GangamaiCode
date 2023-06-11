d5a90b563e7a25799a21038f45332ccc76be6681
pradnyapatil10 <70769442+pradnyapatil10@users.noreply.github.com>
2023-06-11 12:17:15 +0530

    Feedback Page

--- src/app/core/jwt.interceptor.ts
+++ src/app/core/jwt.interceptor.ts
@@ -1,21 +1,29 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import {
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor,
+} from "@angular/common/http";
+import { Observable } from "rxjs";
 
-import { AuthenticationService } from './services/authentication.service';
+import { AuthenticationService } from "./services/authentication.service";
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) { }
+    constructor(private authenticationService: AuthenticationService) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    intercept(
+        request: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
         let currentUser = this.authenticationService.currentUserValue;
         if (currentUser && currentUser.token) {
             request = request.clone({
                 setHeaders: {
-                    Authorization: `Bearer ${currentUser.token}`
-                }
+                    Authorization: `Bearer ${currentUser.token}`,
+                },
             });
         }
         // HIMS Link
@@ -23,12 +31,12 @@ export class JwtInterceptor implements HttpInterceptor {
         // request = request.clone({ url: `http://103.117.208.130:6064/api/${request.url}` });
         //  request = request.clone({ url: `http://103.113.29.249:56/api/${request.url}` });
 
-       
-        
         // Construction Link
-        request = request.clone({ url: `http://103.117.208.130:6063/api/${request.url}` });
+        request = request.clone({
+            url: `http://103.113.29.249:7001/api/${request.url}`,
+        });
         // Local Link
-    //    request = request.clone({ url: `http://localhost:63750/api/${request.url}` });
+        //    request = request.clone({ url: `http://localhost:63750/api/${request.url}` });
         return next.handle(request);
     }
 }
