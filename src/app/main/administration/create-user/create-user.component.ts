aa6edf3d3bbf2f401d2706f24f0726f982663639
VIHIMS <subhash060984@gmail.com>
2023-06-14 11:42:39 +0530

    Create User Page

--- src/app/main/administration/create-user/create-user.component.ts
+++ src/app/main/administration/create-user/create-user.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { fuseAnimations } from '@fuse/animations';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.scss']
+  styleUrls: ['./create-user.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations: fuseAnimations,
 })
 export class CreateUserComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      myInput: new FormControl(''),
+      myInput1: new FormControl(''),
+      myInput2: new FormControl('')
+    });
+
+  }
+  ngAfterViewInit() {
+    this.renderer.selectRootElement('#myInput').focus();
   }
 
+  focusMyNextInput() {
+    this.renderer.selectRootElement('#myInput1').focus();
+
+  }
+  focusMyNextInput2() {
+    this.renderer.selectRootElement('#myInput2').focus();
+
+  }
+  focusMyNextInput3() {
+    this.renderer.selectRootElement('#Btnsave').focus();
+
+  }
 }
