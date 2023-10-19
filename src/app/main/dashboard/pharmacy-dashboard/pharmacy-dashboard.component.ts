bc3c01cdd2ff3978a2c67677f7fd31f53cb255b7
VIHIMS <subhash060984@gmail.com>
2023-10-19 17:49:58 +0530

    PharmacyDashboard

--- src/app/main/dashboard/pharmacy-dashboard/pharmacy-dashboard.component.ts
+++ src/app/main/dashboard/pharmacy-dashboard/pharmacy-dashboard.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { DashboardService } from '../dashboard.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { fuseAnimations } from '@fuse/animations';
 
 @Component({
   selector: 'app-pharmacy-dashboard',
   templateUrl: './pharmacy-dashboard.component.html',
-  styleUrls: ['./pharmacy-dashboard.component.scss']
+  styleUrls: ['./pharmacy-dashboard.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations: fuseAnimations,
 })
 export class PharmacyDashboardComponent implements OnInit {
-
-  constructor() { }
+  SearchForm: FormGroup;
+  PharDashSalSummary=[];
+  constructor(
+    public _pharService:DashboardService,
+    private _formBuilder: FormBuilder
+  ) {
+    this.SearchForm=this.SearchFilter();
+   }
 
   ngOnInit(): void {
+    this.getPharDashboardSalesSummary();
   }
 
+  SearchFilter():FormGroup{
+    return this._formBuilder.group({
+      startdate: [(new Date()).toISOString()],
+      enddate: [(new Date()).toISOString()],
+      // StoreId :'',
+    })
+  }
+
+
+  getPharDashboardSalesSummary(){
+    let reqParam = {
+      FromDate: '10/01/2023',
+      ToDate:'10/18/2023'
+    }
+    this._pharService.getPharDashboardSalesSummary(reqParam).subscribe(data => {
+      console.log(data);
+      this.PharDashSalSummary = data as PharDashSummary [];
+    });
+  }
+
+}
+
+export class PharDashSummary {
+  StoreName: number;
+  TotalSales: String;
+  TotalDisc: number;
+  TotalNetSales: String;
+
+  constructor(PharDashSummary) {
+    this.StoreName = PharDashSummary.StoreName || '';
+    this.TotalSales = PharDashSummary.TotalSales || 0;
+    this.TotalDisc = PharDashSummary.TotalDisc || 0;
+    this.TotalNetSales = PharDashSummary.TotalNetSales || 0;
+  }
 }
