2b5486d0ec3a859b729529c40e0bd4e286ce98f4
Vimal <elaunch.vimal@gmail.com>
2023-11-05 10:13:53 +0530

    Add Simple report

--- src/app/main/reports/reports.module.ts
+++ src/app/main/reports/reports.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SimplereportComponent } from './simplereport/simplereport.component';
 
 
+const appRoutes: Routes = [
+    {
+        path: "simplereport",
+        loadChildren: () =>
+            import("./simplereport/simplereport.module").then(
+                (m) => m.SimpleReportModule
+            ),
+    },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [],
+    imports: [
+        CommonModule, RouterModule.forChild(appRoutes)
+    ]
 })
 export class ReportsModule { }
