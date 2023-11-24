281a415d66873696ef4b64950c7b34b9e7a6d88d
VIHIMS <subhash060984@gmail.com>
2023-11-24 10:39:10 +0530

    Update Env Files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,24 @@
 export const environment = {
     production: true,
-    hmr       : false
+    hmr       : false,
+    MERCHANT_ID: '29610',
+    SECURITY_TOKEN: 'a4c9741b-2889-47b8-be2f-ba42081a246e',
+    IMEI: 'TEST1001270',
+    MERCHANT_STORE_POS_CODE: '1221258270',
+    AUTO_CANCEL_DURATION_MINUTES: 5,
+    SEQUENCE_NUMBER: 1,
+    ALLOWED_PAYMENT_MODE: '1',
 };
+
+// export const environment = {
+//     production: true,
+//     hmr       : false,
+//     MERCHANT_ID: '29610',
+//     SECURITY_TOKEN: 'a4c9741b-2889-47b8-be2f-ba42081a246e',
+//     IMEI: 'TEST1001270',
+//     MERCHANT_STORE_POS_CODE: '1221258270',
+//     AUTO_CANCEL_DURATION_MINUTES: 5,
+//     SEQUENCE_NUMBER: 1,
+//     ALLOWED_PAYMENT_MODE: '1',
+
+// };
