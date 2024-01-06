a27b17b8a5e076d0e56510b9186c9a2a813c8e09
Ambadas <ambadasgajul1999@gmail.com>
2024-01-06 18:44:28 +0530

    update

--- src/app/main/inventory/item-movemnent/item-movemnent.component.ts
+++ src/app/main/inventory/item-movemnent/item-movemnent.component.ts
@@ -96,7 +96,7 @@ export class ItemMovemnentComponent implements OnInit {
       "FromDate": this.datePipe.transform(this._ItemMovemnentService.ItemSearchGroup.get("start").value, "yyyy-MM-dd 00:00:00.000") || '01/01/1900',
       "ToDate": this.datePipe.transform(this._ItemMovemnentService.ItemSearchGroup.get("end").value, "yyyy-MM-dd 00:00:00.000") || '01/01/1900',
       "FromStoreID": this._ItemMovemnentService.ItemSearchGroup.get('StoreId').value.storeid || 1,
-      'ItemId': this._ItemMovemnentService.ItemSearchGroup.get('ItemID').value.ItemID || this.ItemId
+      'ItemId': this._ItemMovemnentService.ItemSearchGroup.get('ItemID').value.ItemID || 0
     }
     console.log(vdata);
     this._ItemMovemnentService.getItemMovementList(vdata).subscribe(data => {
