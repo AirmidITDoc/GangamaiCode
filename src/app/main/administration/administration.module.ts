a91dad8b037a411e6e91d3a0750c12b6547e0880
Vimal <elaunch.vimal@gmail.com>
2024-01-11 20:30:06 +0530

    set permission popup init UI

--- src/app/main/administration/administration.module.ts
+++ src/app/main/administration/administration.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { RolePermissionComponent } from './role-permission/role-permission.component';
  
    
  
@@ -59,7 +60,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  declarations: [   ],
+  declarations: [  RolePermissionComponent ],
   imports: [
     RouterModule.forChild(appRoutes),
   ]
