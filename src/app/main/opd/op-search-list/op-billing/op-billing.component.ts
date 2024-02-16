e5e8bd42ab252e4830d3751c1490c81c5eed44aa
VIHIMS <subhash060984@gmail.com>
2024-02-16 11:36:54 +0530

    Updated Bill

--- src/app/main/opd/op-search-list/op-billing/op-billing.component.ts
+++ src/app/main/opd/op-search-list/op-billing/op-billing.component.ts
@@ -23,6 +23,7 @@ import { OpPaymentNewComponent } from '../op-payment-new/op-payment-new.componen
 import { PdfviewerComponent } from 'app/main/pdfviewer/pdfviewer.component';
 import { RegInsert } from '../../appointment/appointment.component';
 import { ToastrService } from 'ngx-toastr';
+import { MatSelect } from '@angular/material/select';
 
 type NewType = Observable<any[]>;
 export class ILookup {
@@ -1185,7 +1186,6 @@ debugger
   @ViewChild('doctorname') doctorname: ElementRef;
   @ViewChild('addbutton', { static: true }) addbutton: HTMLButtonElement;
   @ViewChild('netamt') netamt: ElementRef;
-
   @ViewChild('Doctor') Doctor: MatSelect;
 
   onEnterservice(event): void {
