b0508aa3f8a2ef14dd127d9d7891090c76965d2b
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2024-02-27 22:52:47 +0530

    userdetail,Pharmaitemsummary

--- src/app/main/administration/user-detail/user-detail.component.ts
+++ src/app/main/administration/user-detail/user-detail.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { ReplaySubject, Subject } from 'rxjs';
+import { Observable, ReplaySubject, Subject } from 'rxjs';
 import { AdministrationService } from '../administration.service';
 import { AuthenticationService } from 'app/core/services/authentication.service';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { takeUntil } from 'rxjs/operators';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { map, startWith, takeUntil } from 'rxjs/operators';
 import { BatchAndExpDateAdjustmentService } from 'app/main/inventory/batch-and-exp-date-adjustment/batch-and-exp-date-adjustment.service';
 import { fuseAnimations } from '@fuse/animations';
 import Swal from 'sweetalert2';
+import { ToastrService } from 'ngx-toastr';
+import { AdvanceDataStored } from 'app/main/ipd/advance';
 
 @Component({
   selector: 'app-user-detail',
@@ -25,35 +27,30 @@ export class UserDetailComponent implements OnInit {
   RoleList:any=[];
   DoctortypecmbList:any=[];
   UserForm:FormGroup;
+  filteredOptionsRole: Observable<string[]>;
+  filteredOptionsDoctorName: Observable<string[]>;
+  isRoleSelected: boolean = false;
+  isDoctorSelected: boolean = false;
+  vUserId:any=0;
+  registerObj = new UserDetail({});
    msg:any;
    
    snackmessage:any;
     screenFromString = 'admission-form';
-   //doctorone filter
-public doctorFilterCtrl: FormControl = new FormControl();
-public filteredDoctor: ReplaySubject<any> = new ReplaySubject<any>(1);
-
-//role filter
-public roleFilterCtrl: FormControl = new FormControl();
-public filteredRole: ReplaySubject<any> = new ReplaySubject<any>(1);
-
-//role filter
-public storeFilterCtrl: FormControl = new FormControl();
-public filteredStore: ReplaySubject<any> = new ReplaySubject<any>(1);
-
-private _onDestroy = new Subject<void>();
+ 
   isLoading: string;
   
   
 
   constructor( public _UserService: AdministrationService,
     private accountService: AuthenticationService,
-      // public notification:NotificationServiceService,
+    public toastr: ToastrService,
       public _matDialog: MatDialog,
       public _BatchAndExpDateAdjustmentService: BatchAndExpDateAdjustmentService,
       private _loggedService: AuthenticationService,
       public dialogRef: MatDialogRef<UserDetailComponent>,
-      private _formBuilder: FormBuilder
+      private _formBuilder: FormBuilder,
+      @Inject(MAT_DIALOG_DATA) public data: any,
 
   ) { }
 
@@ -61,26 +58,32 @@ private _onDestroy = new Subject<void>();
 
     this.UserForm = this.createPesonalForm();
 
+    if (this.data) {
+      this.registerObj = this.data.registerObj;
+      console.log(this.registerObj);
+      this.vUserId= this.registerObj.UserId;
+    }
+
     this.getDoctorlist1();
     this. getRoleNamelist1();
-   this.gePharStoreList();
-    this.getRolelist();
-    this.getDoctorlist();
+     this.gePharStoreList();
+    // this.getRolelist();
+    // this.getDoctorlist();
+
+    debugger
+    this.filteredOptionsRole = this.UserForm.get('RoleId').valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterRole(value)),
+      
+    );
 
     
-   
-
-    // this.roleFilterCtrl.valueChanges
-    // .pipe(takeUntil(this._onDestroy))
-    // .subscribe(() => {
-    //   this.filterRole();
-    // });
     
-    // this.doctorFilterCtrl.valueChanges
-    // .pipe(takeUntil(this._onDestroy))
-    // .subscribe(() => {
-    //   this.filterDoctor();
-    // });
+    this.filteredOptionsDoctorName = this.UserForm.get('DoctorId').valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterDoctor(value)),
+      
+    );
   }
 
   createPesonalForm() {
@@ -93,6 +96,7 @@ private _onDestroy = new Subject<void>();
       MailId: '',
       MailDomain: '',
       DoctorId: '',
+      RoleId:'',
       RoleName:'',
       Status: '',
       poverify: '',
@@ -112,52 +116,6 @@ private _onDestroy = new Subject<void>();
   }
 
 
-  
-   // doctorone filter code  
-   private filterDoctor() {
-
-    if (!this.DoctortypecmbList) {
-      return;
-    }
-    // get the search keyword
-    let search = this.doctorFilterCtrl.value;
-    if (!search) {
-      this.filteredDoctor.next(this.DoctortypecmbList.slice());
-      return;
-    }
-    else {
-      search = search.toLowerCase();
-    }
-    // filter
-    this.filteredDoctor.next(
-      this.DoctortypecmbList.filter(bank => bank.DoctorName.toLowerCase().indexOf(search) > -1)
-    );
-}
-  
-  
-
-
-   // role filter code  
-   private filterRole() {
-
-    if (!this.RoleList) {
-      return;
-    }
-    // get the search keyword
-    let search = this.roleFilterCtrl.value;
-    if (!search) {
-      this.filteredRole.next(this.RoleList.slice());
-      return;
-    }
-    else {
-      search = search.toLowerCase();
-    }
-    // filter
-    this.filteredRole.next(
-      this.RoleList.filter(bank => bank.RoleName.toLowerCase().indexOf(search) > -1)
-    );
-}
-
  
 gePharStoreList() {
   var vdata = {
@@ -172,37 +130,153 @@ gePharStoreList() {
 }
   getRolelist(){
     this._UserService.getRoleCombobox().subscribe(data => { this.RoleList = data; 
-      this.filteredRole.next(this.RoleList.slice());
+      // this.filteredRole.next(this.RoleList.slice());
     })
   }
   
   getDoctorlist(){
     this._UserService.getDoctorMasterCombo().subscribe(data => { this.DoctortypecmbList = data; 
-      this.filteredDoctor.next(this.DoctortypecmbList.slice());
+      // this.filteredDoctor.next(this.DoctortypecmbList.slice());
     })
   }
   DocotorList:any = [];
   RoleNameList:any = [];
-  getDoctorlist1(){
+  // getDoctorlist1(){
+  //   this._UserService.getDoctorMasterCombo().subscribe(data => {
+  //      this.DocotorList = data; 
+  //      //console.log(this.DocotorList);
+    
+  //   })
+  // }
+
+
+  getDoctorlist1() {
+      
     this._UserService.getDoctorMasterCombo().subscribe(data => {
-       this.DocotorList = data; 
-       //console.log(this.DocotorList);
+      this.DocotorList = data;
+      if (this.data) {
+        const ddValue = this.DocotorList.filter(c => c.DoctorID == this.registerObj.DoctorID);
+        this.UserForm.get('DoctorId').setValue(ddValue[0]);
+      this.UserForm.updateValueAndValidity();
+        return;
+      } 
+    });
     
-    })
-  }
-  getRoleNamelist1(){
-    this._UserService.getRoleCombobox().subscribe(data => {
-       this.RoleNameList = data; 
-       console.log(this.RoleNameList);
-    
-    })
   }
  
+  
+  private _filterDoctor(value: any): string[] {
+    if (value) {
+      const filterValue = value && value.DoctorName ? value.DoctorName.toLowerCase() : value.toLowerCase();
+
+      return this.DocotorList.filter(option => option.DoctorName.toLowerCase().includes(filterValue));
+    }
+
+  }
+
+  getOptionTextDoctorName(option) {
+    return option && option.DoctorName ? option.DoctorName : '';
+
+  }
+  // getRoleNamelist1(){
+  //   this._UserService.getRoleCombobox().subscribe(data => {
+  //      this.RoleNameList = data; 
+  //      console.log(this.RoleNameList);
+    
+  //   })
+  // }
+ 
+
+  getRoleNamelist1() {
+      debugger
+    this._UserService.getRoleCombobox().subscribe(data => {
+      this.RoleNameList = data;
+      console.log(data)
+      if (this.data) {
+        const ddValue = this.RoleNameList.filter(c => c.RoleId == this.registerObj.RoleId);
+        this.UserForm.get('RoleId').setValue(ddValue[0]);
+      this.UserForm.updateValueAndValidity();
+        return;
+      } 
+    });
+    
+  }
+ 
+  
+  private _filterRole(value: any): string[] {
+    if (value) {
+      const filterValue = value && value.RoleName ? value.RoleName.toLowerCase() : value.toLowerCase();
+
+      return this.RoleNameList.filter(option => option.RoleName.toLowerCase().includes(filterValue));
+    }
+
+  }
+
+  getOptionTextRoleName(option) {
+    return option && option.RoleName ? option.RoleName : '';
+
+  }
 
 
+  @ViewChild('fname') fname: ElementRef;
+  @ViewChild('lname') lname: ElementRef;
+  @ViewChild('loginname') loginname: ElementRef;
+  @ViewChild('password') password: ElementRef;
+  @ViewChild('mailid') mailid: ElementRef;
+  @ViewChild('role') role: ElementRef;
+ 
+  @ViewChild('docname') docname: ElementRef;
+  // @ViewChild('city') city: ElementRef;
+  // @ViewChild('Fax') Fax: ElementRef;
 
+  
 
+  onEnterfname(event){
+    if (event.which === 13) {
+      this.lname.nativeElement.focus();
+     
+    }
+  }
 
+  onEnterlname(event){
+    if (event.which === 13) {
+      this.loginname.nativeElement.focus();
+     
+    }
+  }
+
+  onEnterloginname(event){
+    if (event.which === 13) {
+      this.password.nativeElement.focus();
+     
+    }
+  }
+
+  onEnterpassword(event){
+    if (event.which === 13) {
+      this.mailid.nativeElement.focus();
+     
+    }
+  }
+  onEntermailid(event){
+    if (event.which === 13) {
+      this.role.nativeElement.focus();
+     
+    }
+  }
+
+  onEnterrole(event){
+    if (event.which === 13) {
+      this.docname.nativeElement.focus();
+     
+    }
+  }
+  onEnterdocName(event){
+    if (event.which === 13) {
+      // this.suppliertype.nativeElement.focus();
+     
+    }
+  }
 
 
 
@@ -212,62 +286,113 @@ gePharStoreList() {
   }
 
   Save() {
+
+    if(this.vUserId==0){
     this.isLoading = 'submit';
 
   var m_data = {
-    "insertUserDetail": {
-      "UserId": 0,
-      "UserName":  '',
+    "loginInsert": {
       "FirstName": this.UserForm.get('FirstName').value || '',
       "LastName": this.UserForm.get('LastName').value || '',
-      "LoginName": this.UserForm.get('LoginName').value || '',
+      "userName": this.UserForm.get('LoginName').value || '',
       "Password": this.UserForm.get('Password').value || 0,
+      "addedBy":this._loggedService.currentUserValue.user.id,
+       "isActive":true,
       "StoreId": this.UserForm.get('StoreId').value.StoreId || 0,
       "RoleId": this.UserForm.get('RoleName').value.RoleId || 0,
-      "MailDomain":  this.UserForm.get('MailDomain').value || 0,
-      "DoctorId":  this.UserForm.get('DoctorId').value.DoctorId || 0,
+      "isDoctorType":true,
+      "doctorID":  this.UserForm.get('DoctorId').value.DoctorId || 0,
       "Status": this.UserForm.get('Status').value || '',
-      "poverify": this.UserForm.get('poverify').value || 0,
-      "Ipoverify": this.UserForm.get('Ipoverify').value || 0,
-      "Grnverify": this.UserForm.get('Grnverify').value || 0,
-      "Indentverify": this.UserForm.get('Indentverify').value || 0,
-      "IIverify": this.UserForm.get('IIverify').value || 0,
+      "isPOVerify": this.UserForm.get('poverify').value || 0,
+      "isPOInchargeVerify": this.UserForm.get('Ipoverify').value || 0,
+      "isGRNVerify": this.UserForm.get('Grnverify').value || 0,
+      "isBedStatus":  this.UserForm.get('BedStatus').value.DoctorId || 0,
+      "isCollection":true,
+      "isIndentVerify": this.UserForm.get('Indentverify').value || 0,
+      "isInchIndVfy": this.UserForm.get('IIverify').value || 0,
       "CollectionInformation": this.UserForm.get('CollectionInformation').value || '',
-      "CurrentStock": this.UserForm.get('CurrentStock').value || 0,
-      "PatientInformation": this.UserForm.get('PatientInformation').value || 0,
-      "ViewBrowseBill": this.UserForm.get('ViewBrowseBill').value || 0,
-      "IsAddChargeDelete": this.UserForm.get('IsAddChargeDelete').value || 0,
+      "isCurrentStk": this.UserForm.get('CurrentStock').value || 0,
+      "isPatientInfo": this.UserForm.get('PatientInformation').value || 0,
+      "isDateInterval":true,
+      "isDateIntervalDays":0,
+      "mailId": this.UserForm.get('MailId').value || 0,
+      "MailDomain":  this.UserForm.get('MailDomain').value || 0,
+     // "ViewBrowseBill": this.UserForm.get('ViewBrowseBill').value || 0,
+      "addChargeIsDelete": this.UserForm.get('IsAddChargeDelete').value || 0,
       "IsPharmacyBalClearnace": this.UserForm.get('IsPharmacyBalClearnace').value || 0,
-      "BedStatus":  this.UserForm.get('BedStatus').value.DoctorId || 0,
+     
       
     }
   }
-  // this._UserService.UserInsert(m_data).subscribe(response => {
-  //   if (response) {
-  //     this.myFunction("UserDetail Data  saved', 'error !");
-  //         this._matDialog.closeAll();
-  //   } else {
-  //     this.myFunction("UserDetail Data  not saved', 'error !");
-  //     // Swal.fire('Error !', 'Register Data  not saved', 'error');
-  //   }
-  // });
 
   console.log(m_data);
 
   this._UserService.UserInsert(m_data).subscribe(response => {
     console.log(response);
     if (response) {
-      Swal.fire('Congratulations !', 'New Prescription Saved Successfully  !', 'success').then((result) => {
-        if (result.isConfirmed) {
-          this._matDialog.closeAll();
-        }   
+      this.toastr.success('User Detail Save', 'Save !', {
+        toastClass: 'tostr-tost custom-toast-success',
       });
     } else {
-      Swal.fire('Error !', 'Prescription Not Updated', 'error');
+      this.toastr.error('API Error!', 'Error !', {
+        toastClass: 'tostr-tost custom-toast-error',
+      });
     }
-    this.isLoading = '';
   });
+    }
+    else{
 
+      var m_data1 = {
+        "loginUpdate": {
+          "UserId":this.vUserId,
+          "FirstName": this.UserForm.get('FirstName').value || '',
+          "LastName": this.UserForm.get('LastName').value || '',
+          "userName": this.UserForm.get('LoginName').value || '',
+          // "Password": this.UserForm.get('Password').value || 0,
+          "addedBy":this._loggedService.currentUserValue.user.id,
+           "isActive":true,
+          "StoreId":this._loggedService.currentUserValue.user.storeId || this.UserForm.get('StoreId').value.StoreId || 0,
+          "RoleId": this.UserForm.get('RoleId').value.RoleId || 0,
+          "isDoctorType":true,
+          "doctorID":  this.UserForm.get('DoctorId').value.DoctorId || 0,
+          "Status": this.UserForm.get('Status').value || '',
+          "isPOVerify": this.UserForm.get('poverify').value || 0,
+          "isPOInchargeVerify": this.UserForm.get('Ipoverify').value || 0,
+          "isGRNVerify": this.UserForm.get('Grnverify').value || 0,
+          "isBedStatus":  this.UserForm.get('BedStatus').value.DoctorId || 0,
+          "isCollection":true,
+          "isIndentVerify": this.UserForm.get('Indentverify').value || 0,
+          "isInchIndVfy": this.UserForm.get('IIverify').value || 0,
+          "CollectionInformation": this.UserForm.get('CollectionInformation').value || '',
+          "isCurrentStk": this.UserForm.get('CurrentStock').value || 0,
+          "isPatientInfo": this.UserForm.get('PatientInformation').value || 0,
+          "isDateInterval":true,
+          "isDateIntervalDays":0,
+          "mailId": this.UserForm.get('MailId').value || 0,
+          "MailDomain":  this.UserForm.get('MailDomain').value || 0,
+         // "ViewBrowseBill": this.UserForm.get('ViewBrowseBill').value || 0,
+          "addChargeIsDelete": this.UserForm.get('IsAddChargeDelete').value || 0,
+          "IsPharmacyBalClearnace": this.UserForm.get('IsPharmacyBalClearnace').value || 0,
+         
+          
+        }
+      }
+    
+      console.log(m_data1);
+    
+      this._UserService.UserUpdate(m_data1).subscribe(response => {
+        console.log(response);
+        if (response) {
+          this.toastr.success('User Detail Update', 'Save !', {
+            toastClass: 'tostr-tost custom-toast-success',
+          });
+        } else {
+          this.toastr.error('API Error!', 'Error !', {
+            toastClass: 'tostr-tost custom-toast-error',
+          });
+        }
+      });
+    }
 }
 
 
@@ -285,7 +410,7 @@ myFunction(s) {
 export class UserDetail {
   UserId: any;
   UserName: any;
-  LoginName:  any;
+  UserLoginName:  any;
   Password: any;
   StoreId: any;
   RoleId:  any;
@@ -297,7 +422,33 @@ export class UserDetail {
   Grnverify:  any;
   Indentverify: any;
   IIverify:  any;
- 
+  DoctorID:any;
+    
+    FirstName:any;
+    LastName:any;
+    IsActive:any;
+    AddedBy:any;
+    RoleName:any;
+    
+    StoreName:any;
+    IsDoctorType:any;
+    
+    DoctorName:any;
+    IsPOVerify:any;
+    IsGRNVerify:any;
+    IsCollection:any;
+    IsBedStatus:any;
+    IsCurrentStk:any;
+    IsPatientInfo:any;
+    IsDateInterval:any;
+    IsDateIntervalDays:any;
+    MailId:any;
+    
+    AddChargeIsDelete:any;
+    IsIndentVerify:any;
+    IsInchIndVfy:any;
+
+  
   /**
    * Constructor
    *
@@ -308,12 +459,13 @@ export class UserDetail {
     {
       this.UserId = UserDetail.UserId || '';
       this.UserName = UserDetail.UserName || '';
-      this.LoginName = UserDetail.LoginName || '';
+      this.UserLoginName = UserDetail.UserLoginName || '';
       this.Password = UserDetail.Password || 0;
       this.StoreId = UserDetail.StoreId || '';
       this.RoleId = UserDetail.RoleId || '';
       this.MailDomain = UserDetail.MailDomain || '';
       this.DoctorId = UserDetail.DoctorId || 0;
+      this.DoctorID = UserDetail.DoctorID || 0;
       this.Status = UserDetail.Status || '';
       this.poverify = UserDetail.poverify || '';
       this.Ipoverify = UserDetail.Ipoverify || '';
@@ -321,8 +473,31 @@ export class UserDetail {
 
       this.Indentverify = UserDetail.Indentverify || '';
       this.IIverify = UserDetail.IIverify || '';
+      this.FirstName= UserDetail.Indentverify || '';
+      this.LastName= UserDetail.Indentverify || '';
+      this.IsActive= UserDetail.Indentverify || '';
+      this.AddedBy= UserDetail.Indentverify || '';
+      this.RoleName= UserDetail.Indentverify || '';
+      
+      this.StoreName= UserDetail.Indentverify || '';
+      this.IsDoctorType= UserDetail.Indentverify || '';
+      
+      this.DoctorName= UserDetail.Indentverify || '';
+      this.IsPOVerify= UserDetail.Indentverify || '';
+      this.IsGRNVerify= UserDetail.Indentverify || '';
+      this.IsCollection= UserDetail.Indentverify || '';
+      this.IsBedStatus= UserDetail.Indentverify || '';
+      this.IsCurrentStk= UserDetail.Indentverify || '';
+      this.IsPatientInfo= UserDetail.Indentverify || '';
+      this.IsDateInterval= UserDetail.Indentverify || '';
+      this.IsDateIntervalDays= UserDetail.Indentverify || '';
+      this.MailId= UserDetail.Indentverify || '';
+      
+      this.AddChargeIsDelete= UserDetail.Indentverify || '';
+      this.IsIndentVerify= UserDetail.Indentverify || '';
+      this.IsInchIndVfy= UserDetail.Indentverify || '';
      
     }
-  }
-}
+  
+}}
 
