b69d2db5e0dd0d492339b48e1ddb326f63b39479
Shivam Kawale <ultimateengineer2002@gmail.com>
2024-05-11 00:23:55 +0530

    fixed checkboxes issue

--- src/app/main/setup/billing/service-master/service-master.service.ts
+++ src/app/main/setup/billing/service-master/service-master.service.ts
@@ -113,12 +113,10 @@ export class ServiceMasterService {
     populateForm(param) {
         this.myform.patchValue(param);
         this.edit_data = param;
-        this.myform.get("CreditedtoDoctor").setValue(param.CreditedtoDoctor);
-        this.myform.get("IsActive").setValue(param.IsActive);
-        this.myform.get("IsEditable").setValue(param.IsEditable);
-        this.myform.get("IsEmergency").setValue(param.IsEmergency);
-        this.myform.get("IsPathology").setValue(param.IsPathology == 1 ? true : false);
-        this.myform.get("IsRadiology").setValue(param.IsRadiology == 1 ? true : false);
-
+        this.myform.get("IsPathology").setValue(param.IsPathology == "1" ? true : false);
+        this.myform.get("IsRadiology").setValue(param.IsRadiology == "1" ? true : false);
+        this.myform.get("CreditedtoDoctor").setValue(param.CreditedtoDoctor == "true" ? true : false);
+        this.myform.get("IsEditable").setValue(param.IsEditable == "true" ? true : false);
+        this.myform.get("IsEmergency").setValue(param.IsEmergency == "true" ? true : false);
     }
 }
