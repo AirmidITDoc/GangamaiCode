f05d8da90ffae86ea29aa38913b7b981d18c5a6b
Ambadas <ambadasgajul1999@gmail.com>
2024-07-04 10:58:18 +0530

    prescription

--- src/app/main/nursingstation/prescription/new-prescription/new-prescription.component.ts
+++ src/app/main/nursingstation/prescription/new-prescription/new-prescription.component.ts
@@ -448,7 +448,7 @@ export class NewPrescriptionComponent implements OnInit {
         ItemName: this.ItemForm.get('ItemId').value.ItemName || '',
         DoseName: this.ItemForm.get('DoseId').value.DoseName || '',
         DoseId: this.ItemForm.get('DoseId').value.DoseId || '',
-        Day: this.vDay,
+        Days: this.vDay,
         Instruction: this.vInstruction || '' 
       });
     this.dsItemList.data = this.Chargelist
@@ -582,7 +582,7 @@ export class NewPrescriptionComponent implements OnInit {
       insertIP_Prescription['genericId'] = 1;
       insertIP_Prescription['drugId'] = element.ItemID;
       insertIP_Prescription['doseId'] =  element.DoseId || 0;
-      insertIP_Prescription['days'] = element.Day || 0;
+      insertIP_Prescription['days'] = element.Days || 0;
       insertIP_Prescription['qtyPerDay'] =0,// element.Qty || 0
       insertIP_Prescription['totalQty'] = 0,//element.Qty || 0;
       insertIP_Prescription['remark'] = element.Instruction || '';
