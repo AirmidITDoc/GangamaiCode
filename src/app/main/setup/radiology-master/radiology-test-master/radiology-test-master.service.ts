83dd7ab4194a365f2b307797a59f68403db9bdc4
AirmidITDoc <rairmiditdoc@gmail.com>
2024-07-13 16:44:45 +0530

    Radiology Design changes

--- src/app/main/setup/radiology-master/radiology-test-master/radiology-test-master.service.ts
+++ src/app/main/setup/radiology-master/radiology-test-master/radiology-test-master.service.ts
@@ -8,12 +8,12 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class RadiologyTestMasterService {
   myform: FormGroup;
   myformSearch: FormGroup;
-  AddParameterFrom:FormGroup;
+  AddParameterFrom: FormGroup;
 
-  constructor(private _httpClient: HttpClient,private _formBuilder: FormBuilder) {
-    this.myform=this.createRadiologytestForm();
+  constructor(private _httpClient: HttpClient, private _formBuilder: FormBuilder) {
+    this.myform = this.createRadiologytestForm();
     this.myformSearch = this.createSearchForm();
-    this.AddParameterFrom =this.createAddparaFrom();
+    this.AddParameterFrom = this.createAddparaFrom();
   }
 
   createRadiologytestForm(): FormGroup {
@@ -21,11 +21,11 @@ export class RadiologyTestMasterService {
       TestId: [''],
       TestName: [''],
       PrintTestName: [''],
-      CategoryId:[''],
+      CategoryId: [''],
       //TemplateName:[''],
-      ServiceId:[''],
+      ServiceId: [''],
       IsDeleted: ['true'],
-     
+
     });
   }
   createSearchForm(): FormGroup {
@@ -33,45 +33,43 @@ export class RadiologyTestMasterService {
       TestNameSearch: [""],
       IsDeletedSearch: ["2"],
     });
-}
-createAddparaFrom(): FormGroup {
-  return this._formBuilder.group({
-    TestId:[''],
-    TemplateName: [""]
-  });
-}
+  }
+  createAddparaFrom(): FormGroup {
+    return this._formBuilder.group({
+      TestId: [''],
+      TemplateName: [""]
+    });
+  }
 
   initializeFormGroup() {
     this.createRadiologytestForm();
     this.createSearchForm();
   }
   // get Test Master list
-  public getRadiologyList(param) { 
-    return this._httpClient.post( "Generic/GetByProc?procName=Retrieve_RadiologyTestList",
-        param
-    );
-}
-  
+  public getRadiologyList(param) {
+    return this._httpClient.post("Generic/GetDataSetByProc?procName=m_Rtrv_RadiologyTestList", param);
+  }
+
 
   // Category Master Combobox List
   public getCategoryMasterCombo() {
     return this._httpClient.post("Generic/GetByProc?procName=Retrieve_RadiologyCategoryMasterForCombo", {})
   }
 
-  public insertRadiologyTestMaster(employee) { 
+  public insertRadiologyTestMaster(employee) {
     return this._httpClient.post("RadiologyMaster/RadiologyTestMasterSave", employee);
   }
-  
+
   // Service Master Combobox List
   public getServiceMasterCombo() {
     return this._httpClient.post("Generic/GetByProc?procName=Retrieve_RathTestListForCombo", {})
   }
-    
+
   // TemplateMaster Combobox List dropdown
   public gettemplateMasterCombo() {
     return this._httpClient.post("Generic/GetByProc?procName=Retrieve_RadiologyTemplateMasterForComboMaster", {})
   }
-    
+
   // TemplateMaster Combobox List
   public gettemplateMasterComboList(param) {
     return this._httpClient.post("Generic/GetByProc?procName=Retrieve_RadiologyTemplateMasterForComboMasterList", param)
