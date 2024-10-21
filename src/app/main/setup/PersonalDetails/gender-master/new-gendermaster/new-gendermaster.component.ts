c084309179b2937d36a47a65f7e74bf36278d327
Vimal Vataliya <vimal@comnet.com.sg>
2024-10-21 21:58:50 +0530

    update validation

--- src/app/main/setup/PersonalDetails/gender-master/new-gendermaster/new-gendermaster.component.ts
+++ src/app/main/setup/PersonalDetails/gender-master/new-gendermaster/new-gendermaster.component.ts
@@ -4,30 +4,34 @@ import { ToastrService } from 'ngx-toastr';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-new-gendermaster',
-  templateUrl: './new-gendermaster.component.html',
-  styleUrls: ['./new-gendermaster.component.scss']
+    selector: 'app-new-gendermaster',
+    templateUrl: './new-gendermaster.component.html',
+    styleUrls: ['./new-gendermaster.component.scss']
 })
 export class NewGendermasterComponent implements OnInit {
 
-  constructor(public _GenderMasterService: GenderMasterService,
-    public dialogRef: MatDialogRef<NewGendermasterComponent>,
-    public toastr: ToastrService, public _matDialog: MatDialog) { }
+    constructor(public _GenderMasterService: GenderMasterService,
+        public dialogRef: MatDialogRef<NewGendermasterComponent>,
+        public toastr: ToastrService, public _matDialog: MatDialog) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+    genderName = [{ name: "required", Message: "Gender Name is required" },
+    { name: "maxlength", Message: "Gender name should not be greater than 50 char." },
+    { name: "pattern", Message: "Special char not allowed." }
+    ];
+    onSubmit() {
+        debugger
+        if (this._GenderMasterService.myform.valid) {
+            console.log('Form Submitted:', this._GenderMasterService.myform.value);
+        } else {
+            console.log('Form is invalid.');
+        }
+    }
 
-  onSubmit(){
-    if (this._GenderMasterService.myform.valid) {
-        console.log('Form Submitted:', this._GenderMasterService.myform.value);
-      } else {
-        console.log('Form is invalid.');
-      }
-  }
+    close() {
+        this.dialogRef.close()
+    }
 
-  close(){
-this.dialogRef.close()
-  }
-
-  onClear(){}
+    onClear() { }
 }
--- src/app/main/shared/componets/airmid-textbox/airmid-textbox.component.ts
+++ src/app/main/shared/componets/airmid-textbox/airmid-textbox.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, forwardRef } from "@angular/core";
 import {
     ControlValueAccessor,
     FormControl,
+    FormGroup,
     NG_VALUE_ACCESSOR,
     Validators
 } from "@angular/forms";
@@ -12,54 +13,58 @@ import {
     styleUrls: ["./airmid-textbox.component.scss"],
     providers: [
         {
-          provide: NG_VALUE_ACCESSOR,
-          useExisting: forwardRef(() => AirmidTextboxComponent),
-          multi: true,
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => AirmidTextboxComponent),
+            multi: true,
         },
-      ],    
+    ],
 })
-export class AirmidTextboxComponent implements ControlValueAccessor, OnInit {
-    constructor() {}
+export class AirmidTextboxComponent implements OnInit {
+    constructor() { }
     @Input() label: string = ""; // Dynamic placeholder
     @Input() placeholder: string = "Enter text"; // Dynamic placeholder
-    @Input() maxLength: number = 30; // Maximum length
-    @Input() required: boolean = false; // Required validation
+    @Input() maxLength: number = 50; // Maximum length
+    @Input() formGroup: FormGroup;
+    @Input() fieldName: string = '';
+    @Input() validations: [] = [];
+    @Input() disabled: boolean = false;
 
-    control: FormControl = new FormControl(""); // Form control to manage value
+    //control: FormControl = new FormControl(""); // Form control to manage value
 
-    onChange: any = () => {};
-    onTouched: any = () => {};
+    // onChange: any = () => {};
+    onTouched: any = () => { };
 
     // Called when Angular wants to write a value to the control
-    writeValue(value: any): void {
-        this.control.setValue(value);
-    }
+    // writeValue(value: any): void {
+    //     this.control.setValue(value);
+    // }
 
-    // Registers a callback function to be called when the control's value changes
-    registerOnChange(fn: any): void {
-        this.onChange = fn;
-        this.control.valueChanges.subscribe(fn); // Emit value changes
-    }
+    // // Registers a callback function to be called when the control's value changes
+    // registerOnChange(fn: any): void {
+    //     this.onChange = fn;
+    //     this.control.valueChanges.subscribe(fn); // Emit value changes
+    // }
 
-    // Registers a callback function to be called when the control is touched
+    // // Registers a callback function to be called when the control is touched
     registerOnTouched(fn: any): void {
         this.onTouched = fn;
     }
 
     // Set component's disabled state
-    setDisabledState?(isDisabled: boolean): void {
-        isDisabled ? this.control.disable() : this.control.enable();
-    }
+    // setDisabledState?(isDisabled: boolean): void {
+    //     isDisabled ? this.control.disable() : this.control.enable();
+    // }
 
     // Handle custom validation if required is enabled
     ngOnInit() {
-        if (this.required) {
-            this.control.setValidators([
-                Validators.required,
-                Validators.maxLength(this.maxLength),
-            ]);
-        } else {
-            this.control.setValidators([Validators.maxLength(this.maxLength)]);
-        }
+        debugger
+        // if (this.required) {
+        //     this.control.setValidators([
+        //         Validators.required,
+        //         Validators.maxLength(this.maxLength),
+        //     ]);
+        // } else {
+        //     this.control.setValidators([Validators.maxLength(this.maxLength)]);
+        // }
     }
 }
