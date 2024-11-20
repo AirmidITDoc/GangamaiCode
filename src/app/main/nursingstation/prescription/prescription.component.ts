a4c72e8d4ff5ec2328d9277cb46a3519631ea313
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2024-11-20 22:10:48 +0530

    bed,discharge,prescription,labrequest,appointment

--- src/app/main/nursingstation/prescription/prescription.component.ts
+++ src/app/main/nursingstation/prescription/prescription.component.ts
@@ -42,10 +42,10 @@ export class PrescriptionComponent implements OnInit {
 
   
   gridConfig: gridModel = {
-      apiUrl: "Nursing/LabRequestList",
+      apiUrl: "Nursing/PrescriptionList",
       columnsList: [
-          { heading: "Code", key: "pbillNo", sort: true, align: 'left', emptySign: 'NA' ,width:50},
-          { heading: "Patient Name", key: "patientName", sort: true, align: 'left', emptySign: 'NA' ,width:250},
+          { heading: "Code", key: "requestId", sort: true, align: 'left', emptySign: 'NA' ,width:50},
+          { heading: "Item Name", key: "itemName", sort: true, align: 'left', emptySign: 'NA' ,width:250},
           // { heading: "BillTime", key: "billTime", sort: true, align: 'left', emptySign: 'NA' ,width:150},
           // { heading: "MobileNo", key: "mobileNo", sort: true, align: 'left', emptySign: 'NA',width:50 },
           // { heading: "DoctorName", key: "doctorName", sort: true, align: 'left', emptySign: 'NA' ,width:150},
@@ -86,9 +86,9 @@ export class PrescriptionComponent implements OnInit {
       sortField: "RequestId",
       sortOrder: 0,
       filters: [
-          { fieldName: "FromDate", fieldValue: "01/01/2023", opType: OperatorComparer.Equals },
-          { fieldName: "ToDate", fieldValue: "11/01/2024", opType: OperatorComparer.Equals },
-          { fieldName: "Reg_No", fieldValue: "0", opType: OperatorComparer.Equals },
+        //   { fieldName: "FromDate", fieldValue: "01/01/2023", opType: OperatorComparer.Equals },
+          { fieldName: "RequestId", fieldValue: "0", opType: OperatorComparer.Equals },
+        //   { fieldName: "Reg_No", fieldValue: "0", opType: OperatorComparer.Equals },
           { fieldName: "Start", fieldValue: "0", opType: OperatorComparer.Equals },
           { fieldName: "Length", fieldValue: "30", opType: OperatorComparer.Equals }
          // { fieldName: "isActive", fieldValue: "", opType: OperatorComparer.Equals }
@@ -96,7 +96,60 @@ export class PrescriptionComponent implements OnInit {
       row: 25
   }
 
-
+  gridConfig1: gridModel = {
+    apiUrl: "Nursing/LabRequestList",
+    columnsList: [
+        { heading: "Code", key: "requestId", sort: true, align: 'left', emptySign: 'NA' ,width:50},
+        { heading: "Item Name", key: "patientName", sort: true, align: 'left', emptySign: 'NA' ,width:250},
+        // { heading: "BillTime", key: "billTime", sort: true, align: 'left', emptySign: 'NA' ,width:150},
+        // { heading: "MobileNo", key: "mobileNo", sort: true, align: 'left', emptySign: 'NA',width:50 },
+        // { heading: "DoctorName", key: "doctorName", sort: true, align: 'left', emptySign: 'NA' ,width:150},
+        // { heading: "VisitDate", key: "visitDate", sort: true, align: 'left', emptySign: 'NA' ,width:150},
+        // { heading: "DepartmentName", key: "departmentName", sort: true, align: 'left', emptySign: 'NA' ,width:150},
+        // { heading: "TotalAmt", key: "totalAmt", sort: true, align: 'left', emptySign: 'NA',width:50 },
+        // { heading: "Net Pay", key: "netPayableAmt", sort: true, align: 'left', emptySign: 'NA' ,width:50},
+        
+        {
+            heading: "Action", key: "action", align: "right", type: gridColumnTypes.action, actions: [
+                {
+                    action: gridActions.edit, callback: (data: any) => {
+                        this.onSave(data);
+                    }
+                }, {
+                    action: gridActions.delete, callback: (data: any) => {
+                        this.confirmDialogRef = this._matDialog.open(
+                            FuseConfirmDialogComponent,
+                            {
+                                disableClose: false,
+                            }
+                        );
+                        this.confirmDialogRef.componentInstance.confirmMessage = "Are you sure you want to deactive?";
+                        this.confirmDialogRef.afterClosed().subscribe((result) => {
+                            if (result) {
+                                let that = this;
+                                this._PrescriptionService.deactivateTheStatus(data.RequestId).subscribe((response: any) => {
+                                    this.toastr.success(response.message);
+                                    that.grid.bindGridData();
+                                });
+                            }
+                            this.confirmDialogRef = null;
+                        });
+                    }
+                }]
+        } //Action 1-view, 2-Edit,3-delete
+    ],
+    sortField: "RequestId",
+    sortOrder: 0,
+    filters: [
+      //   { fieldName: "FromDate", fieldValue: "01/01/2023", opType: OperatorComparer.Equals },
+        { fieldName: "RequestId", fieldValue: "0", opType: OperatorComparer.Equals },
+      //   { fieldName: "Reg_No", fieldValue: "0", opType: OperatorComparer.Equals },
+        { fieldName: "Start", fieldValue: "0", opType: OperatorComparer.Equals },
+        { fieldName: "Length", fieldValue: "30", opType: OperatorComparer.Equals }
+       // { fieldName: "isActive", fieldValue: "", opType: OperatorComparer.Equals }
+    ],
+    row: 25
+}
   constructor(public _PrescriptionService: PrescriptionService, public _matDialog: MatDialog,
       public toastr : ToastrService,) {}
   ngOnInit(): void {
