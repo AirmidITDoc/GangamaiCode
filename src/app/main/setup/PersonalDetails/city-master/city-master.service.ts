8e9345e5dde568b66de5a1098c8a4d0990207abd
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2024-11-26 11:48:26 +0530

    city

--- src/app/main/setup/PersonalDetails/city-master/city-master.service.ts
+++ src/app/main/setup/PersonalDetails/city-master/city-master.service.ts
@@ -59,6 +59,6 @@ export class CityMasterService {
     }
 
     public deactivateTheStatus(m_data) {
-        return this._httpClient.PostData("CityMaster", m_data);
+        return this._httpClient.DeleteData("CityMaster?Id=" + m_data.toString());
     }
 }
