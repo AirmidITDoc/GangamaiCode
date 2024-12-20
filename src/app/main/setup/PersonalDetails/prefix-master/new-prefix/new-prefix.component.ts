1e11e449e0784434d61a4cdb34fd6b4933f1ad74
Vimal Vataliya <vimal@comnet.com.sg>
2024-12-20 19:25:22 +0530

    solve isactive issue in prefix page

--- src/app/main/setup/PersonalDetails/prefix-master/new-prefix/new-prefix.component.ts
+++ src/app/main/setup/PersonalDetails/prefix-master/new-prefix/new-prefix.component.ts
@@ -6,18 +6,20 @@ import { FuseConfirmDialogComponent } from '@fuse/components/confirm-dialog/conf
 import { gridModel } from 'app/core/models/gridRequest';
 import { FormGroup } from '@angular/forms';
 import { fuseAnimations } from '@fuse/animations';
+import { isatty } from 'tty';
 
 
 @Component({
-  selector: 'app-new-prefix',
-  templateUrl: './new-prefix.component.html',
-  styleUrls: ['./new-prefix.component.scss'],
-  encapsulation: ViewEncapsulation.None,
-  animations: fuseAnimations,
+    selector: 'app-new-prefix',
+    templateUrl: './new-prefix.component.html',
+    styleUrls: ['./new-prefix.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations,
 })
 export class NewPrefixComponent implements OnInit {
     prefixForm: FormGroup;
-    autocompleteModegender:string="Gender";
+    isActive:boolean=false;
+    autocompleteModegender: string = "Gender";
     constructor(
         public _PrefixMasterService: PrefixMasterService,
         public dialogRef: MatDialogRef<NewPrefixComponent>,
@@ -28,60 +30,62 @@ export class NewPrefixComponent implements OnInit {
 
     ngOnInit(): void {
         this.prefixForm = this._PrefixMasterService.createPrefixForm();
-        if(this.data){
-        var m_data = {
-            prefixId: this.data?.prefixId,
-            prefixName: this.data?.prefixName,
-            sexId:this.data?.sexId,
-            isActive: JSON.stringify(this.data?.isActive),
-        };
-        this.prefixForm.patchValue(m_data);
-    }else{
-        var m_data1 = {
-            prefixId:0,
-            prefixName:"",
-            sexId: 0,
-            isActive:1,
-        };
-        this.prefixForm.patchValue(m_data1);
-    }
-  
+        debugger
+        if (this.data) {
+            var m_data = {
+                prefixId: this.data?.prefixId,
+                prefixName: this.data?.prefixName,
+                sexId: this.data?.sexId,
+                isActive: JSON.stringify(this.data?.isActive),
+            };
+            this.isActive=this.data.isActive;
+            this.prefixForm.patchValue(m_data);
+        } else {
+            var m_data1 = {
+                prefixId: 0,
+                prefixName: "",
+                sexId: 0,
+                isActive: 1,
+            };
+            this.prefixForm.patchValue(m_data1);
+        }
+
     }
 
-    saveflag : boolean = false;
+    saveflag: boolean = false;
     onSubmit() {
-        this.saveflag = true; 
-        
+        this.saveflag = true;
+
         if (this.prefixForm.invalid) {
             this.toastr.warning('please check from is invalid', 'Warning !', {
-              toastClass:'tostr-tost custom-toast-warning',
-          })
-          return;
-        }else{
-                var mdata=
-                {
-                  "prefixId": this.prefixForm.value?.prefixId || 0,
-                  "prefixName": this.prefixForm.value?.prefixName,
-                  "sexId":parseInt(this.prefixForm.value?.sexId)
-                }
-                console.log("prefix json:", mdata);
-                
-                this._PrefixMasterService.prefixMasterSave(mdata).subscribe((response) => {
-                    this.toastr.success(response.message);
-                    this.onClear(true);
-                }, (error) => {
-                    this.toastr.error(error.message);
-                });
-      }
-    }
-    genderId=0;
+                toastClass: 'tostr-tost custom-toast-warning',
+            })
+            return;
+        } else {
+            var mdata =
+            {
+                "prefixId": this.prefixForm.value?.prefixId || 0,
+                "prefixName": this.prefixForm.value?.prefixName,
+                "sexId": parseInt(this.prefixForm.value?.sexId)
+            }
+            console.log("prefix json:", mdata);
 
-    selectChangegender(obj:any){
-        console.log(obj)
-        this.genderId=obj.value;
+            this._PrefixMasterService.prefixMasterSave(mdata).subscribe((response) => {
+                this.toastr.success(response.message);
+                this.onClear(true);
+            }, (error) => {
+                this.toastr.error(error.message);
+            });
+        }
     }
-    
-    getValidationGenderMessages(){
+    genderId = 0;
+
+    selectChangegender(obj: any) {
+        console.log(obj)
+        this.genderId = obj.value;
+    }
+
+    getValidationGenderMessages() {
         return {
             GenderId: [
                 { name: "required", Message: "Gender is required" }
