bb3c9f4c3e60ec93bde7235f7f2816fc7dca415e
talabhavdip <talabhavdip@gmail.com>
2025-01-21 19:49:41 +0530

    Working on new registraion page and new appointment page.

--- src/app/main/opd/registration/registration.component.ts
+++ src/app/main/opd/registration/registration.component.ts
@@ -116,7 +116,7 @@ export class RegistrationComponent implements OnInit {
         const dialogRef = this._matDialog.open(NewRegistrationComponent,
             {
                 maxWidth: "95vw",
-                height: '95%',
+                // height: '95%',
                 width: '90%',
 
             });
