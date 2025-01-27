9f5a209b89e6d74765722795b544441c30228088
Vimal Vataliya <elaunch.vimal@gmail.com>
2025-01-27 21:15:50 +0530

    Add autocomeplete in appoinment page

--- src/app/main/opd/appointment-list/appointmentlist.service.ts
+++ src/app/main/opd/appointment-list/appointmentlist.service.ts
@@ -5,150 +5,153 @@ import { LoaderService } from 'app/core/components/loader/loader.service';
 import { ApiCaller } from 'app/core/services/apiCaller';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AppointmentlistService {
-  myCrossConsulteForm: FormGroup;
-  myformSearch: FormGroup;
-  personalFormGroup: FormGroup;
-  VisitFormGroup: FormGroup;
+    myCrossConsulteForm: FormGroup;
+    myformSearch: FormGroup;
+    personalFormGroup: FormGroup;
+    VisitFormGroup: FormGroup;
 
-  constructor(public _httpClient1: ApiCaller,private _formBuilder: UntypedFormBuilder,   private _loaderService: LoaderService,
-    public _httpClient: HttpClient,
-  ) {  
-    this.myformSearch=this.filterForm();
-    this.myCrossConsulteForm = this.createConsultatDrForm(); 
+    constructor(public _httpClient1: ApiCaller, private _formBuilder: UntypedFormBuilder, private _loaderService: LoaderService,
+        public _httpClient: HttpClient,
+    ) {
+        this.myformSearch = this.filterForm();
+        this.myCrossConsulteForm = this.createConsultatDrForm();
     }
 
-  
-  // new APi
-  
-  filterForm(): FormGroup {
-    return this._formBuilder.group({
-      RegNo:'',
-      FirstName:['', [
-        Validators.pattern("^[A-Za-z]*[a-zA-Z]*$"),
-      ]],
-      LastName:['', [
-        Validators.pattern("^[A-Za-z]*[a-zA-Z]*$"),
-      ]],
-      DoctorId:'',
-      DoctorName:'',
-      IsMark: 2,
-      fromDate: [(new Date()).toISOString()],
-      enddate: [(new Date()).toISOString()],
-    });
-  }
-createSearchForm(): FormGroup {
-  return this._formBuilder.group({
-      patientNameSearch: [""],
-      IsDeletedSearch: ["2"],
-  });
-}
+
+    // new APi
+
+    filterForm(): FormGroup {
+        return this._formBuilder.group({
+            RegNo: '',
+            FirstName: ['', [
+                Validators.pattern("^[A-Za-z]*[a-zA-Z]*$"),
+            ]],
+            LastName: ['', [
+                Validators.pattern("^[A-Za-z]*[a-zA-Z]*$"),
+            ]],
+            DoctorId: '',
+            DoctorName: '',
+            IsMark: 2,
+            fromDate: [(new Date()).toISOString()],
+            enddate: [(new Date()).toISOString()],
+        });
+    }
+    createSearchForm(): FormGroup {
+        return this._formBuilder.group({
+            patientNameSearch: [""],
+            IsDeletedSearch: ["2"],
+        });
+    }
 
 
-createConsultatDrForm() {
-  return this._formBuilder.group({
-    visitId:0,
-    regId:0,
-    consultantDocId: '',
-    departmentId: ''
-  });
-}
+    createConsultatDrForm() {
+        return this._formBuilder.group({
+            visitId: 0,
+            regId: 0,
+            consultantDocId: '',
+            departmentId: ''
+        });
+    }
 
-createRefranceDrForm() {
-  return this._formBuilder.group({
-    visitId:0,
-    // regId:0,
-    refDocId: ['', [
-      Validators.required]],
-   
-  });
-}
+    createRefranceDrForm() {
+        return this._formBuilder.group({
+            visitId: 0,
+            // regId:0,
+            refDocId: ['', [
+                Validators.required]],
 
-initializeFormGroup() {
-  // this.createPesonalForm();
-  // this.createVisitdetailForm();
-}
+        });
+    }
 
- 
-public documentuploadInsert(employee, loader = true){
-  if (loader) {
-      this._loaderService.show();
-  }
-  return this._httpClient1.PutData("InPatient/DocAttachment", employee);
-}
+    initializeFormGroup() {
+        // this.createPesonalForm();
+        // this.createVisitdetailForm();
+    }
 
-public NewappointmentSave(Param: any, showLoader = true) {
-    // if (Param.visitID) {
+
+    public documentuploadInsert(employee, loader = true) {
+        if (loader) {
+            this._loaderService.show();
+        }
+        return this._httpClient1.PutData("InPatient/DocAttachment", employee);
+    }
+
+    public NewappointmentSave(Param: any, showLoader = true) {
+        // if (Param.visitID) {
         return this._httpClient1.PostData("VisitDetail/AppVisitInsert", Param, showLoader);
-    // } else return this._httpClient1.PostData("VisitDetail/AppVisitInsert", Param, showLoader);
-}
+        // } else return this._httpClient1.PostData("VisitDetail/AppVisitInsert", Param, showLoader);
+    }
 
-public RregisteredappointmentSave(Param: any, showLoader = true) {
- 
-      return this._httpClient1.PostData("VisitDetail/Update",Param, showLoader);
-  
-  // else return this._httpClient1.PostData("VisitDetail/AppVisitInsert", Param, showLoader);
-}
+    public RregisteredappointmentSave(Param: any, showLoader = true) {
 
-public EditConDoctor(Param: any, showLoader = true) {
+        return this._httpClient1.PostData("VisitDetail/Update", Param, showLoader);
 
-   return this._httpClient1.PutData("ConsRefDoctor/Edit/" + Param.visitId, Param, showLoader);
-}
+        // else return this._httpClient1.PostData("VisitDetail/AppVisitInsert", Param, showLoader);
+    }
 
-public EditRefDoctor(Param: any, showLoader = true) {
- 
-  return this._httpClient1.PostData("ConsRefDoctor/RefDoctorUpdate", Param, showLoader);
-}
+    public EditConDoctor(Param: any, showLoader = true) {
 
-public deactivateTheStatus(m_data) {
-    return this._httpClient1.PostData("VisitDetail", m_data);
-}
+        return this._httpClient1.PutData("ConsRefDoctor/Edit/" + Param.visitId, Param, showLoader);
+    }
 
-public crossconsultSave(Param: any, showLoader = true) {
-  return this._httpClient1.PostData("CrossConsultation/CrossConsultationInsert", Param, showLoader);
-  
-}
+    public EditRefDoctor(Param: any, showLoader = true) {
 
+        return this._httpClient1.PostData("ConsRefDoctor/RefDoctorUpdate", Param, showLoader);
+    }
 
-  
+    public deactivateTheStatus(m_data) {
+        return this._httpClient1.PostData("VisitDetail", m_data);
+    }
+
+    public crossconsultSave(Param: any, showLoader = true) {
+        return this._httpClient1.PostData("CrossConsultation/CrossConsultationInsert", Param, showLoader);
+
+    }
 
 
-// new API?
 
-public getAppointmentList(employee) {
-  return this._httpClient1.PostData("VisitDetail/AppVisitList", employee)
-}  
 
-public Appointmentcancle(employee, loader = true) {
 
-  return this._httpClient1.PostData("VisitDetail/Cancel", employee);
-}
+    // new API?
 
-public getdoctorList(employee) {
-  return this._httpClient1.PostData("DoctoreMaster/List",employee)
-}  
+    public getAppointmentList(employee) {
+        return this._httpClient1.PostData("VisitDetail/AppVisitList", employee)
+    }
 
-public getVisitById(Id,showLoader = true) {
-  return this._httpClient1.GetData("VisitDetail/" + Id,showLoader);
-}
-public getPatientListView(mode){
-  return this._httpClient1.PostData("Report/ViewReport",mode);
+    public Appointmentcancle(employee, loader = true) {
 
-}
+        return this._httpClient1.PostData("VisitDetail/Cancel", employee);
+    }
 
-public getMaster(mode,Id) {
-  return this._httpClient1.GetData("Dropdown/GetBindDropDown?mode="+mode+"&Id="+Id);
-}
+    public getdoctorList(employee) {
+        return this._httpClient1.PostData("DoctoreMaster/List", employee)
+    }
 
-  public getAppointmenttemplateReport(Param: any, showLoader = true) {
-    return this._httpClient1.PostData("Report/ViewReport",Param,showLoader);
-  }
+    public getVisitById(Id, showLoader = true) {
+        return this._httpClient1.GetData("VisitDetail/" + Id, showLoader);
+    }
+    public getPatientListView(mode) {
+        return this._httpClient1.PostData("Report/ViewReport", mode);
 
-  public getRegistraionById(Id,showLoader = true) {
-    return this._httpClient1.GetData("OutPatient/" + Id,showLoader);
-}
+    }
+
+    public getMaster(mode, Id) {
+        return this._httpClient1.GetData("Dropdown/GetBindDropDown?mode=" + mode + "&Id=" + Id);
+    }
+
+    public getAppointmenttemplateReport(Param: any, showLoader = true) {
+        return this._httpClient1.PostData("Report/ViewReport", Param, showLoader);
+    }
+
+    public getRegistraionById(Id, showLoader = true) {
+        return this._httpClient1.GetData("OutPatient/" + Id, showLoader);
+    }
+    public getRegistrations(keyword, showLoader = true) {
+        return this._httpClient1.GetData("OutPatient/auto-complete?Keyword=" + keyword, showLoader);
+    }
 }
 
 //192.168.2.100:
