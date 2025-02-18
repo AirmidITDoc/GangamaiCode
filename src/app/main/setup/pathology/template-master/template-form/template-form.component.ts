929feaccf5a73424d699b93d6e85c8a7decfa440
Vimal Vataliya <elaunch.vimal@gmail.com>
2025-02-18 21:40:12 +0530

    Set html editor

--- src/app/main/setup/pathology/template-master/template-form/template-form.component.ts
+++ src/app/main/setup/pathology/template-master/template-form/template-form.component.ts
@@ -14,34 +14,61 @@ import { FormGroup } from '@angular/forms';
     animations: fuseAnimations,
 })
 export class TemplateFormComponent implements OnInit {
-    
+
     templateForm: FormGroup;
 
-    vTemplateDesc:any;
+    vTemplateDesc: any;
+    // editorConfig: AngularEditorConfig = {
+    // editable: true,
+    // spellcheck: true,
+    // height: '20rem',
+    // minHeight: '20rem',
+    // translate: 'yes',
+    // placeholder: 'Enter text here...',
+    // enableToolbar: true,
+    // showToolbar: true,
+    // };
     editorConfig: AngularEditorConfig = {
-    editable: true,
-    spellcheck: true,
-    height: '20rem',
-    minHeight: '20rem',
-    translate: 'yes',
-    placeholder: 'Enter text here...',
-    enableToolbar: true,
-    showToolbar: true,
+        editable: true,
+        spellcheck: true,
+        height: '15rem',
+        minHeight: '5rem',
+        placeholder: 'Enter text here...',
+        translate: 'no',
+        defaultParagraphSeparator: '',
+        defaultFontName: 'Arial', enableToolbar: true,
+        showToolbar: true,
+        sanitize: false,
+        toolbarPosition: 'top',
+        customClasses: [
+            {
+                name: "quote",
+                class: "quote",
+            },
+            {
+                name: 'redText',
+                class: 'redText'
+            },
+            {
+                name: "titleText",
+                class: "titleText",
+                tag: "h1",
+            },
+        ]
     };
-
-    vTemplateName:any;
+    vTemplateName: any;
     TemplateId = 0;
-    vTemplateDescInHtml:any;
+    vTemplateDescInHtml: any;
     constructor(
         public _TemplateServieService: TemplateServieService,
         public dialogRef: MatDialogRef<TemplateFormComponent>,
         @Inject(MAT_DIALOG_DATA) public data: any,
         public toastr: ToastrService
     ) { }
-   
+
     ngOnInit(): void {
         this.templateForm = this._TemplateServieService.createTemplateForm();
-        
+
         if (this.data) {
             // var m_data = {
             //     templateId: this.data?.templateId,
@@ -50,68 +77,67 @@ export class TemplateFormComponent implements OnInit {
             //     templateDescInHtml: this.data?.templateDescInHtml?.trim(),
             // };
             // this.templateForm.patchValue(m_data);
-            this.vTemplateName=this.data.templateName;
-            this.vTemplateDesc=this.data.templateDesc;
-            this.TemplateId=this.data.templateId;
-            this.vTemplateDescInHtml=this.data.templateDescInHtml;
+            this.vTemplateName = this.data.templateName;
+            this.vTemplateDesc = this.data.templateDesc;
+            this.TemplateId = this.data.templateId;
+            this.vTemplateDescInHtml = this.data.templateDescInHtml;
         } else {
             console.warn("Data is not available in ngOnInit.");
         }
     }
-    
+
     onSubmit() {
 
-            if (!this.TemplateId) {
-                
-                var mdata={
-                      "templateId": 0,
-                      "templateName": this.templateForm.get("templateName").value,
-                      "templateDesc": this.templateForm.get("templateDesc").value,
-                      "templateDescInHtml": "string"              
-                }
-                console.log('json mdata:',mdata);
+        if (!this.TemplateId) {
 
-                  this._TemplateServieService.templateMasterSave(mdata).subscribe((response) => {
-                      this.toastr.success(response.message);
-                      this.onClear();
-                  }, (error) => {
-                      this.toastr.error(error.message);
-                  });
-              }
-              else{
-                var mdata={
-                    "templateId": this.TemplateId,
-                    "templateName": this.templateForm.get("templateName").value,
-                    "templateDesc": this.templateForm.get("templateDesc").value,
-                    "templateDescInHtml": "string"              
-              }
-              console.log('json mdata:',mdata);
+            var mdata = {
+                "templateId": 0,
+                "templateName": this.templateForm.get("templateName").value,
+                "templateDesc": this.templateForm.get("templateDesc").value,
+                "templateDescInHtml": "string"
+            }
+            console.log('json mdata:', mdata);
 
-                this._TemplateServieService.templateMasterSave(mdata).subscribe((response) => {
-                    this.toastr.success(response.message);
-                    this.onClear();
-                }, (error) => {
-                    this.toastr.error(error.message);
-                });
-              }
-        
+            this._TemplateServieService.templateMasterSave(mdata).subscribe((response) => {
+                this.toastr.success(response.message);
+                this.onClear();
+            }, (error) => {
+                this.toastr.error(error.message);
+            });
+        }
+        else {
+            var mdata = {
+                "templateId": this.TemplateId,
+                "templateName": this.templateForm.get("templateName").value,
+                "templateDesc": this.templateForm.get("templateDesc").value,
+                "templateDescInHtml": "string"
+            }
+            console.log('json mdata:', mdata);
+
+            this._TemplateServieService.templateMasterSave(mdata).subscribe((response) => {
+                this.toastr.success(response.message);
+                this.onClear();
+            }, (error) => {
+                this.toastr.error(error.message);
+            });
+        }
+
         this.onClose();
     }
-    onClose(){
+    onClose() {
         this.templateForm.reset();
         this.dialogRef.close();
     }
 
-    onClear(){}
+    onClear() { }
 
     onBlur(e: any) {
-    throw new Error('Method not implemented.');
-    // this.vTemplateDesc = e.target.innerHTML;
+        throw new Error('Method not implemented.');
+        // this.vTemplateDesc = e.target.innerHTML;
     }
 
     // onBlur(e: any) {
     //     this.vConsentText = e.target.innerHTML;
     //   }
-    
+
 }
-  
