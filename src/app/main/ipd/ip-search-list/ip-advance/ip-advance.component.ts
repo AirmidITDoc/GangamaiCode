1f5f711f67cd9c4064bf4bed1354eafa413b41a9
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2025-03-25 21:57:21 +0530

    adv

--- src/app/main/ipd/ip-search-list/ip-advance/ip-advance.component.ts
+++ src/app/main/ipd/ip-search-list/ip-advance/ip-advance.component.ts
@@ -271,10 +271,10 @@ export class IPAdvanceComponent implements OnInit {
            console.log(response)
           this.toastr.success(response.message);
           this.grid.bindGridData();
-          let Res = response.message
-          let ID = Res.split('.')
-          let Id = ID[1] 
-          this.viewgetAdvanceReceiptReportPdf(Id);
+          // let Res = response.message
+          // let ID = Res.split('.')
+          // let Id = ID[1] 
+          this.viewgetAdvanceReceiptReportPdf(response.data);
           this.getWhatsappsAdvance(response, this.vMobileNo);
           this._matDialog.closeAll();
         }, (error) => {
