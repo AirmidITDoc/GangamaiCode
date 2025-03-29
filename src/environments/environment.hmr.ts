72d081e638e922aaffd52fdfdaa07f08b94fd2ca
Hitesh Patel <hitpatel05@gmail.com>
2025-03-29 13:13:47 +0530

    report field added

--- src/environments/environment.hmr.ts
+++ src/environments/environment.hmr.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    //API_BASE_PATH: 'http://192.168.2.100:9091/api/v1/',
-    API_BASE_PATH: 'http://localhost:5251/api/v1/',
+    API_BASE_PATH: 'http://192.168.2.100:9091/api/v1/',
+    //API_BASE_PATH: 'http://localhost:5251/api/v1/',
     hmr       : true
 };
