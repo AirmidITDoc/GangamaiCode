2983a8a21bbbb8c4691d7038f23e2f1a71c91e05
Ambadas <ambadasgajul1999@gmail.com>
2025-04-03 12:09:49 +0530

    login

--- src/app/main/auth/login/login.component.ts
+++ src/app/main/auth/login/login.component.ts
@@ -9,7 +9,7 @@ import { ServerMonitoringService } from "app/core/services/servermonitoring.serv
 import { setInterval, setTimeout } from "timers";
 import { MatDialog } from "@angular/material/dialog";
 import { AppointmentBillingComponent } from "app/main/opd/appointment-list/appointment-billing/appointment-billing.component";
-import { EncryptionService } from "app/core/services/encryption.service";
+// import { EncryptionService } from "app/core/services/encryption.service";
 
 @Component({
     selector: "login",
@@ -34,7 +34,7 @@ export class LoginComponent implements OnInit {
         private authenticationService: AuthenticationService,
         private serverMonitoringService: ServerMonitoringService,
         private _matDialog: MatDialog,
-        private encryptionService:EncryptionService
+        //private encryptionService:EncryptionService
     ) {
         // Configure the layout
         this._fuseConfigService.config = {
@@ -84,8 +84,8 @@ export class LoginComponent implements OnInit {
         if (this.loginForm.invalid) {
             return;
         }
-        this.obj.Username=this.encryptionService.encrypt(this.obj.Username);
-        this.obj.Password=this.encryptionService.encrypt(this.obj.Password);
+        // this.obj.Username=this.encryptionService.encrypt(this.obj.Username);
+        // this.obj.Password=this.encryptionService.encrypt(this.obj.Password);
         this.authenticationService.login(this.obj).subscribe(
             (data) => {
                 if ((data?.userId ?? 0) > 0) {
