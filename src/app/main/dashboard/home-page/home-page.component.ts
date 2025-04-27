65f4814ca2d70bd3ad7914d6694d668726c32ea5
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2025-04-27 12:46:04 +0530

    Updated Home Page login

--- src/app/main/dashboard/home-page/home-page.component.ts
+++ src/app/main/dashboard/home-page/home-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { fuseAnimations } from '@fuse/animations';
+import { AuthenticationService } from 'app/core/services/authentication.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations: fuseAnimations,
 })
 export class HomePageComponent {
+  username: any;
+  // private currentUserSubject: BehaviorSubject<User>;
+  // public currentUser: Observable<User>;
+
+  constructor(
+        public _accountServices: AuthenticationService,
+        private router: Router,
+  ) { 
+    //  this.currentUserSubject = new BehaviorSubject<User>(
+    //             JSON.parse(localStorage.getItem("currentUser"))
+    //         );
+  }
+
+  ngOnInit(): void {
+    this.username = this._accountServices.currentUserValue.user
+    ? this._accountServices.currentUserValue.user.firstName + ' ' + this._accountServices.currentUserValue.user.lastName
+    : '';
+  }
 
 }
