01aaf30c40a9032693b426af8ee772765853e1af
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2025-05-23 23:08:25 +0530

    sales,

--- src/app/main/pharmacy/sales-return-bill-settlement/sales-return-bill-settlement.service.ts
+++ src/app/main/pharmacy/sales-return-bill-settlement/sales-return-bill-settlement.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UntypedFormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoaderService } from 'app/core/components/loader/loader.service';
+import { ApiCaller } from 'app/core/services/apiCaller';
 
 @Injectable({
   providedIn: 'root'
@@ -9,55 +10,63 @@ import { LoaderService } from 'app/core/components/loader/loader.service';
 export class SalesReturnBillSettlementService {
 
   userFormGroup: FormGroup;
- ItemSubform: FormGroup;
+  ItemSubform: FormGroup;
 
   constructor(
-    public _httpClient: HttpClient,
+    public _httpClient: HttpClient, public _httpClient1: ApiCaller,
     private _formBuilder: UntypedFormBuilder,
     private _loaderService: LoaderService,
-  ) { 
-    this.userFormGroup = this.CreateUseFrom();
-    this.ItemSubform = this.getItemSubform(); 
+  ) {
+    // this.userFormGroup = this.CreateUseFrom();
+    // this.ItemSubform = this.getItemSubform();
   }
 
   CreateUseFrom() {
     return this._formBuilder.group({
       RegID: [''],
-      Op_ip_id: ['1'],
-      advanceAmt:[''],
-      comment:['']
+      PatientType: ['OP'],
+      DoctorName: '',
+      PatientName: '',
+      extAddress: '',
+      advanceAmt: [''],
+      comment: [''],
+      MobileNo: ['', [Validators.required, Validators.pattern("^[0-9]*$"),
+      Validators.minLength(10),
+      Validators.maxLength(10),]],
     });
   }
 
   getItemSubform() {
     return this._formBuilder.group({
-      RegID:'',
+      RegID: '',
       PatientName: '',
       DoctorName: '',
       extAddress: '',
       MobileNo: ['', [Validators.required, Validators.pattern("^[0-9]*$"),
       Validators.minLength(10),
       Validators.maxLength(10),]],
-      PatientType: ['IP'], 
+      PatientType: ['IP'],
     });
   }
-  public getConcessionCombo()
-  {
-    return this._httpClient.post("Generic/GetByProc?procName=Retrieve_ConcessionReasonMasterForCombo", {});
-  } 
 
-  public getAdmittedpatientlist(employee){
+  public getRegistraionById(Id) {
+        return this._httpClient1.GetData("OutPatient/" + Id);
+    }
+
+  public getConcessionCombo() {
+    return this._httpClient.post("Generic/GetByProc?procName=Retrieve_ConcessionReasonMasterForCombo", {});
+  }
+
+  public getAdmittedpatientlist(employee) {
     return this._httpClient.post("Generic/GetByProc?procName=m_Rtrv_PatientAdmittedListSearch ", employee)
   }
-  public getSalesList(Param ,loader = true){ 
-    if (loader) {
-      this._loaderService.show();
+  public getSalesList(Param) {
+  
+    return this._httpClient1.PostData("Sales/PharSalesSettlemet", Param);
   }
-    return this._httpClient.post("Generic/GetByProc?procName=m_Rtrv_Phar_Bill_List_Settlement",Param);
-  }
-  public getItemDetailList(Param){
-    
-    return this._httpClient.post("Generic/GetByProc?procName=Ret_PrescriptionDet",Param);
+  public getItemDetailList(Param) {
+
+    return this._httpClient.post("Generic/GetByProc?procName=Ret_PrescriptionDet", Param);
   }
   public getAdmittedPatientList(employee) {
     return this._httpClient.post("Generic/GetByProc?procName=m_Rtrv_PatientAdmittedListSearch", employee)
@@ -66,10 +75,8 @@ export class SalesReturnBillSettlementService {
   public getPatientVisitedListSearch(employee) {//m_Rtrv_PatientVisitedListSearch
     return this._httpClient.post("Generic/GetByProc?procName=m_Rtrv_PatientVisitedListSearch", employee)
   }
-  public InsertSalessettlement(emp, loader = true) {
-    if (loader) {
-      this._loaderService.show();
-  }
+  public InsertSalessettlement(emp) {
+  
     return this._httpClient.post("Pharmacy/PaymentSettlement", emp);
   }
 }
