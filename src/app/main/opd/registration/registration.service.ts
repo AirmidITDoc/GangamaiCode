4fb04c2cc7cc1bc4cf70748c13ba31a1a939ba42
rakshanetalkar <163313832+rakshanetalkar@users.noreply.github.com>
2025-06-03 19:09:21 +0530

    appointment.phoneappointment,registration

--- src/app/main/opd/registration/registration.service.ts
+++ src/app/main/opd/registration/registration.service.ts
@@ -13,58 +13,60 @@ export class RegistrationService {
     constructor(
         public _httpClient: HttpClient, public _httpClient1: ApiCaller,
         private _formBuilder: UntypedFormBuilder, private _FormvalidationserviceService: FormvalidationserviceService,
-         private accountService: AuthenticationService,
+        private accountService: AuthenticationService,
         private _loaderService: LoaderService
-    ) {}
+    ) { }
 
     filterForm(): FormGroup {
         return this._formBuilder.group({
-            RegNo:['',[this._FormvalidationserviceService.allowEmptyStringValidator()]],
+            RegNo: ['', [this._FormvalidationserviceService.allowEmptyStringValidator()]],
             FirstName: ['', [
-                   Validators.maxLength(50),
+                Validators.maxLength(50),
                 Validators.pattern("^[A-Za-z/() ]*$")
             ]],
             LastName: ['', [
-                    Validators.maxLength(50),
+                Validators.maxLength(50),
                 Validators.pattern("^[A-Za-z/() ]*$")
             ]],
-            fromDate:[(new Date()).toISOString(),this._FormvalidationserviceService.validDateValidator()],
-            enddate:[(new Date()).toISOString(),this._FormvalidationserviceService.validDateValidator()],
-            MobileNo:['', [
+            fromDate: [(new Date()).toISOString(), this._FormvalidationserviceService.validDateValidator()],
+            enddate: [(new Date()).toISOString(), this._FormvalidationserviceService.validDateValidator()],
+            MobileNo: ['', [
                 Validators.minLength(10),
                 Validators.maxLength(10),
                 Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")
-                ]],
+            ]],
         });
     }
 
     //changed by raksha date:6/3/25
     createPesonalForm1() {
         return this._formBuilder.group({
-            RegId:[0,[this._FormvalidationserviceService.onlyNumberValidator()]],
-            RegNo:['',[this._FormvalidationserviceService.allowEmptyStringValidator()]],
-            PrefixId:[0, [Validators.required, this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
+            RegId: [0, [this._FormvalidationserviceService.onlyNumberValidator()]],
+            RegNo: ['', [this._FormvalidationserviceService.allowEmptyStringValidator()]],
+            PrefixId: [0, [Validators.required, this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
             FirstName: ['', [
                 Validators.required,
                 Validators.minLength(1),
                 Validators.maxLength(100),
-                Validators.pattern("^[A-Za-z/() ]*$"),this._FormvalidationserviceService.notBlankValidator(),this._FormvalidationserviceService.allowEmptyStringValidator()
+                Validators.pattern("^[A-Za-z/() ]*$"),
+                this._FormvalidationserviceService.notBlankValidator()
             ]],
             MiddleName: ['', [
-                Validators.minLength(1),
                 Validators.maxLength(100),
-                Validators.pattern("^[A-Za-z/() ]*$")
+                Validators.pattern("^[A-Za-z/() ]*$"),
+                this._FormvalidationserviceService.allowEmptyStringValidator()
             ]],
             LastName: ['', [
                 Validators.required,
                 Validators.minLength(1),
                 Validators.maxLength(100),
-                Validators.pattern("^[A-Za-z/() ]*$"),this._FormvalidationserviceService.notBlankValidator(),this._FormvalidationserviceService.allowEmptyStringValidator()
+                Validators.pattern("^[A-Za-z/() ]*$"),
+                this._FormvalidationserviceService.notBlankValidator()
             ]],
             GenderId: [0, [Validators.required, this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
             // GenderId: new FormControl( [0, [Validators.required, this._FormvalidationserviceService.notEmptyOrZeroValidator()]]),
-            Address:['',[this._FormvalidationserviceService.allowEmptyStringValidator(),Validators.maxLength(200)]],
-            DateOfBirth:[(new Date()).toISOString(),this._FormvalidationserviceService.validDateValidator()],
+            Address: ['', [this._FormvalidationserviceService.allowEmptyStringValidator(), Validators.maxLength(200)]],
+            DateOfBirth: [(new Date()).toISOString(), this._FormvalidationserviceService.validDateValidator()],
             Age: ['0'],
             AgeYear: ['0', [
                 // Validators.required,
@@ -73,33 +75,36 @@ export class RegistrationService {
             AgeMonth: ['0', [Validators.pattern("^[0-9]*$")]],
             AgeDay: ['0', [Validators.pattern("^[0-9]*$")]],
             PhoneNo: ['', [Validators.minLength(10),
-            Validators.maxLength(10),
-            Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"),this._FormvalidationserviceService.onlyNumberValidator()
+                Validators.maxLength(10),
+                Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"),
+                this._FormvalidationserviceService.onlyNumberValidator()
             ]],
             MobileNo: ['', [Validators.required,
-            Validators.minLength(10),
-            Validators.maxLength(10),
-            Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"),this._FormvalidationserviceService.onlyNumberValidator(),this._FormvalidationserviceService.notEmptyOrZeroValidator()
+                Validators.minLength(10),
+                Validators.maxLength(10),
+                Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"),
+                this._FormvalidationserviceService.onlyNumberValidator()
             ]],
             aadharCardNo: ['', [
-            Validators.minLength(12),
-            Validators.maxLength(12),
-            Validators.pattern("^[0-9]*$"),this._FormvalidationserviceService.onlyNumberValidator()
+                Validators.minLength(12),
+                Validators.maxLength(12),
+                Validators.pattern("^[0-9]*$"), 
+                this._FormvalidationserviceService.onlyNumberValidator()
             ]],
 
-            panCardNo:['',[this._FormvalidationserviceService.allowEmptyStringValidator()]],
+            panCardNo: ['', [this._FormvalidationserviceService.allowEmptyStringValidator()]],
             // MaritalStatusId:[0,[this._FormvalidationserviceService.onlyNumberValidator()]],
-            MaritalStatusId:[0,[Validators.required, this._FormvalidationserviceService.onlyNumberValidator(),this._FormvalidationserviceService.notEmptyOrZeroValidator()]], //changed by raksha
-            ReligionId: [0,[this._FormvalidationserviceService.onlyNumberValidator()]],
-            AreaId: [0,[this._FormvalidationserviceService.onlyNumberValidator()]],
-            CityId:[0, [Validators.required, this._FormvalidationserviceService.onlyNumberValidator(),this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
+            MaritalStatusId: [0, [Validators.required, this._FormvalidationserviceService.onlyNumberValidator(), this._FormvalidationserviceService.notEmptyOrZeroValidator()]], //changed by raksha
+            ReligionId: [0, [this._FormvalidationserviceService.onlyNumberValidator()]],
+            AreaId: [0, [this._FormvalidationserviceService.onlyNumberValidator()]],
+            CityId: [0, [Validators.required, this._FormvalidationserviceService.onlyNumberValidator(), this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
             City: [''],
-            StateId:  [0, [Validators.required, this._FormvalidationserviceService.onlyNumberValidator(),this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
-            CountryId:  [0, [Validators.required, this._FormvalidationserviceService.onlyNumberValidator(),this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
+            StateId: [0, [Validators.required, this._FormvalidationserviceService.onlyNumberValidator(), this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
+            CountryId: [0, [Validators.required, this._FormvalidationserviceService.onlyNumberValidator(), this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
             IsCharity: false,
             IsSeniorCitizen: false,
-            AddedBy: [this.accountService.currentUserValue.userId,this._FormvalidationserviceService.onlyNumberValidator()],
-            updatedBy: [this.accountService.currentUserValue.userId,this._FormvalidationserviceService.onlyNumberValidator()],
+            AddedBy: [this.accountService.currentUserValue.userId, this._FormvalidationserviceService.onlyNumberValidator()],
+            updatedBy: [this.accountService.currentUserValue.userId, this._FormvalidationserviceService.onlyNumberValidator()],
             RegDate: [''],
             RegTime: [''],
             Photo: [''],
@@ -109,12 +114,12 @@ export class RegistrationService {
 
     }
     // new Api
-    initializeFormGroup() {}
+    initializeFormGroup() { }
 
     public RegstrationtSaveData(Param: any) {
 
         if (Param.RegId) {
-        return this._httpClient1.PostData("OutPatient/RegistrationUpdate", Param);
+            return this._httpClient1.PostData("OutPatient/RegistrationUpdate", Param);
         } else return this._httpClient1.PostData("OutPatient/RegistrationInsert", Param);
     }
 
@@ -122,7 +127,7 @@ export class RegistrationService {
         return this._httpClient1.PostData("OutPatient/RegistrationInsert", m_data);
     }
 
-   
+
     public getNewRegistrationList(employee) {
         return this._httpClient1.PostData("OutPatient/RegistrationList", employee)
     }
