5eab5de8392c921ffbe978d98ea292ee0f318e61
rakshanetalkar <163313832+rakshanetalkar@users.noreply.github.com>
2025-06-14 19:09:35 +0530

    casePaper, CommonComponent=airmid-chipautocomplete

--- src/app/main/opd/new-casepaper/casepaper.service.ts
+++ src/app/main/opd/new-casepaper/casepaper.service.ts
@@ -19,43 +19,6 @@ export class CasepaperService {
     return this._httpClient.post("Generic/GetByProc?procName=rtrv_CaseparVisitDetails", { "VisitId": visitId });
   }
 
-  public getHistoryList() {
-    return this._httpClient.post("Generic/GetByProc?procName=Rtrv_M_PastHistoryMasterForCombo", {})
-  }
-
- //Deartment Combobox List
- public getDepartmentCombo() {
-  return this._httpClient.post("Generic/GetByProc?procName=RetrieveDepartmentMasterForCombo", {})
-} 
-//Deartment Combobox List
-// public getServiceList(param,loader = true){ 
-//   if (loader) {
-//     this._loaderService.show();
-// }//m_Rtrv_PathRadServiceList"
-//  return this._httpClient.post("Generic/GetByProc?procName=m_Rtrv_ServicesList", param)
-// }
-  //Doctor Master Combobox List
-  public getDoctorMasterCombo(Id) {
-    return this._httpClient.post("Generic/GetByProc?procName=Retrieve_DoctorWithDepartMasterForCombo_Conditional", { "Id": Id })
-  }
-    // Admitted Doctor Master Combobox List
-    public getAdmittedDoctorCombo(param, loader = true) {
-      if (loader) {
-        this._loaderService.show();
-    }
-  
-      return this._httpClient.post("Generic/GetByProc?procName=m_Rtrv_DoctorListMasterForCombo",param)
-    } 
-  // public getDoseList() {
-  //   return this._httpClient.post("Generic/GetByProc?procName=ps_Rtrv_DoseMasterList", {})
-  // }
-
-  public getDrugList(drugValue) {
-    // return this._httpClient.post("Generic/GetByProc?procName=ps_RtvrDrugName", { "ItemName": drugValue })
-    return this._httpClient.post("Generic/GetByProc?procName=Retrieve_ItemName_BalanceQty", { "ItemName": drugValue })
-    
-  }
-
   public onSaveCasepaper(param,loader = true){ 
     if (loader) {
       this._loaderService.show();
@@ -65,54 +28,7 @@ export class CasepaperService {
   public SavePrescriptionTemplate(param){ 
     return this._httpClient1.PostData("OPDPrescriptionMedical/OPTemplateInsert", param);
   }
-  public getTemplate(query) {
-    return this._httpClient.post("Generic/GetBySelectQuery?query=" + query, {})
-  }
-  public getOPDPrecriptionPrint(PrecriptionId) {
-    return this._httpClient.post("Generic/GetByProc?procName=rptOPDPrecriptionPrint ", PrecriptionId)
-  }
-
-  // retrive list when we edit featch table, hightlist
-  // public RtrvPreviousprescriptionDetails(visistId,loader = true) {
-  //   if(loader){
-  //     this._loaderService.show();
-  //   }
-  //   return this._httpClient.post("Generic/GetByProc?procName=Get_PrescriptionDetailsVisitWise",visistId);
-  // }
-
-  public getVisitedList(employee,loader = true) {
-    if(loader){
-      this._loaderService.show();
-    }
-
-    return this._httpClient.post("Generic/GetByProc?procName=rtrv_CaseparVisitDetails", employee)
-  }
-
-  // public getRtrvTestService(visistId,loader = true) {
-  //   if(loader){
-  //     this._loaderService.show();
-  //   }
-  //   return this._httpClient.post("Generic/GetByProc?procName=m_Rtrv_OPRequestList",visistId);
-  // }
-  
-  // registration patient list
-  public getRegistrationList(employee) {
-    return this._httpClient.post("Generic/GetByProc?procName=Rtrv_PatientRegistrationList", employee)
-  }
-
-  public getItemlist(Param, loader = true) {
-    if (loader) {
-      this._loaderService.show();
-  } 
-    return this._httpClient.post("Generic/GetByProc?procName=Retrieve_ItemName_BalanceQty",Param)
-  }
-  public getPatientVisitedListSearch(employee) {
-    return this._httpClient.post("Generic/GetByProc?procName=m_Rtrv_PatientVisitedListSearch", employee)
-  }
-
-  public getIpPrescriptionview(OP_IP_ID,PatientType){
-    return this._httpClient.get("InPatient/view-IP_Prescription?OP_IP_ID=" + OP_IP_ID+"&PatientType="+PatientType);
-  }
+ 
   public getOpPrescriptionview(VisitId,loader = true){ 
     if (loader) {
       this._loaderService.show();
@@ -127,30 +43,10 @@ export class CasepaperService {
   }
     return this._httpClient.get("OutPatient/view-OP_PrescriptionwithoutHeader?VisitId=" + VisitId);
   }
-
-// used for prePrescription list
-  // public getRtrvVisitedList(param, loader = true) {
-  //   if (loader) {
-  //     this._loaderService.show();
-  // } 
-  //   return this._httpClient.post("Generic/GetByProc?procName=m_Rtrv_GetVisitInfo",param)
-  // } 
-  // public getcheifcomplaintList( ) { 
-  //   return this._httpClient.post("Generic/GetByProc?procName=m_Rtrv_CheifcomplaintList",{})
-  // } 
-  public getTemplateList() {
-    return this._httpClient.post("Generic/GetByProc?procName=m_RtrvPresTemplateName_List", {})
-  }
+  
   public getTempPrescriptionList(param) {
     return this._httpClient1.PostData("OPDPrescriptionMedical/OPPrescriptionTemplateList",param)
   } 
-  // used to retrive cheifcom,dignosis,exami data
-  public getRtrvCheifComplaintList(visistId,loader = true) {
-    if(loader){
-      this._loaderService.show();
-    }
-    return this._httpClient.post("Generic/GetByProc?procName=m_Rtrv_OPCasepaperDignosisList",visistId);
-  }
 
   // raksha
   public getRegistraionById(Id) {
