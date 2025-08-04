1d54e51ac04a20a0644e5b413e51a073d87db162
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2025-08-04 22:41:11 +0530

    Registration List Completed

--- src/app/main/opd/registration/registration.component.ts
+++ src/app/main/opd/registration/registration.component.ts
@@ -62,24 +62,18 @@ export class RegistrationComponent implements OnInit {
         { heading: "Patient Name", key: "patientName", sort: true, align: 'left', emptySign: 'NA', width: 250 },
         { heading: "Age", key: "ageYear", sort: true, align: 'left', emptySign: 'NA', width: 50 },
         { heading: "Gender", key: "genderName", sort: true, align: 'left', emptySign: 'NA', },
-
         { heading: "Mobile No", key: "mobileNo", sort: true, align: 'left', emptySign: 'NA' },
-         { heading: "Phone No", key: "phoneNo", sort: true, align: 'left', emptySign: 'NA' },
+        { heading: "Phone No", key: "phoneNo", sort: true, align: 'left', emptySign: 'NA' },
         { heading: "Adddress", key: "address", sort: true, align: 'left', emptySign: 'NA', width: 300 },
         { heading: "Annual Income", key: "annualIncome", sort: true, align: 'left', emptySign: 'NA', },
         { heading: "EmgContactPerson Name", key: "emgContactPersonName", sort: true, align: 'left', emptySign: 'NA',width:150},
         { heading: "Emg MobileNo", key: "emgMobileNo", sort: true, align: 'left', emptySign: 'NA', },
         { heading: "Emg LandlineNo", key: "emgLandlineNo", sort: true, align: 'left', emptySign: 'NA', },
         { heading: "Emg Address", key: "engAddress", sort: true, align: 'left', emptySign: 'NA',width:150 },
-       
-        //  { heading: "AnnualIncome", key: "annualIncome", sort: true, align: 'left', emptySign: 'NA', },
-        // { heading: "Phone No", key: "phoneNo", sort: true, align: 'left', emptySign: 'NA', },
-        // { heading: "Phone No", key: "phoneNo", sort: true, align: 'left', emptySign: 'NA', },
-        { heading: "Created Date", key: "createdDate",  sort: true, align: 'left', emptySign: 'NA', type: 8 },
-        { heading: "Modify Date", key: "modifiedDate",  sort: true, align: 'left', emptySign: 'NA', type: 8 },
         { heading: "Added By", key: "createdBy", sort: true, align: 'left', emptySign: 'NA', },
+        { heading: "Created Date", key: "createdDate",  sort: true, align: 'left', emptySign: 'NA', type: 8 ,width:170},
         { heading: "Updated By", key: "updatedBy", sort: true, align: 'left', emptySign: 'NA', },
-
+        { heading: "Modify Date", key: "modifiedDate",  sort: true, align: 'left', emptySign: 'NA', type: 8 ,width:170},
         {
             heading: "Action", key: "action", align: "right", width: 250, sticky: true, type: gridColumnTypes.template,
             template: this.actionButtonTemplate  // Assign ng-template to the column
