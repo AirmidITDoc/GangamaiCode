c5e374f7dff70731a9bbad1940b264e2c1336bac
rakshanetalkar <163313832+rakshanetalkar@users.noreply.github.com>
2025-08-22 13:39:30 +0530

    otrequest

--- src/app/main/otmanagement/ot-request/ot-request.service.ts
+++ src/app/main/otmanagement/ot-request/ot-request.service.ts
@@ -4,13 +4,13 @@ import { ApiCaller } from "app/core/services/apiCaller";
 import { FormvalidationserviceService } from "app/main/shared/services/formvalidationservice.service";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class OtRequestService {
-    
-  
 
- requestform: FormGroup;
+
+
+    requestform: FormGroup;
     myformSearch: FormGroup;
     constructor(
         private _httpClient: ApiCaller,
@@ -18,34 +18,31 @@ export class OtRequestService {
         private _FormvalidationserviceService: FormvalidationserviceService
     ) {
         this.requestform = this.createRequestForm();
-       // this.myformSearch = this.createSearchForm();
+        // this.myformSearch = this.createSearchForm();
     }
 
     createRequestForm(): FormGroup {
         return this._formBuilder.group({
             otbookingId: [0, [this._FormvalidationserviceService.onlyNumberValidator()]],
-            opIpId:["",[Validators.required,this._FormvalidationserviceService.onlyNumberValidator()]],
-            departmentId:[0, [Validators.required,this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
-            surgeryId:[0, [Validators.required,this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
-            categoryId:[0],
-            siteDescId:["",[Validators.required,this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
-            surgeonId:[0, [Validators.required,this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
-            doctorTypeId:[0, [Validators.required,this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
-            otbookingDate:  [new Date()],
-            otbookingTime: [new Date()],
-            opIpType:  ["OP"],
-            surgeryTypeId:[1,[Validators.required,this._FormvalidationserviceService.onlyNumberValidator()]],
-         
-           surgeryCategoryId:[0, [Validators.required,this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
-            otRequestDate: [new Date(),[Validators.required,this._FormvalidationserviceService.validDateValidator()]],
-            otrequestId: [0],
-            otRequestTime: [new Date(),[Validators.required]],
-             isCancelled: [false],
-        isCancelledBy: [0, [this._FormvalidationserviceService.onlyNumberValidator()]],
-       isCancelledDateTime: ['1900-01-01', [this._FormvalidationserviceService.validDateValidator()]],
+            opIpId: ["", [Validators.required, this._FormvalidationserviceService.onlyNumberValidator()]],
+            departmentId: [0, [Validators.required, this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
+            surgeryId: [0, [Validators.required, this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
+            categoryId: [0,[Validators.required, this._FormvalidationserviceService.notEmptyOrZeroValidator()]], //doctortype value passing here
+            siteDescId: ["", [Validators.required, this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
+            surgeonId: [0, [Validators.required, this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
+            // doctorTypeId: [0, [Validators.required, this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
+            otbookingDate: [new Date()],
+            otbookingTime: [''],
+            opIpType: ["OP"],
+            surgeryTypeId: [0, [Validators.required, this._FormvalidationserviceService.onlyNumberValidator()]],
 
-           
-            
+            surgeryCategoryId: [0, [Validators.required, this._FormvalidationserviceService.notEmptyOrZeroValidator()]],
+            otRequestDate: [new Date(), [Validators.required, this._FormvalidationserviceService.validDateValidator()]],
+            otrequestId: [0],
+            otRequestTime: [new Date(), [Validators.required]],
+            isCancelled: [false],
+            isCancelledBy: [0, [this._FormvalidationserviceService.onlyNumberValidator()]],
+            isCancelledDateTime: ['1900-01-01', [this._FormvalidationserviceService.validDateValidator()]],
         });
     }
     // createSearchForm(): FormGroup {
@@ -56,25 +53,25 @@ export class OtRequestService {
     // }
 
     getOtRequestList(fromDate: string, toDate: string) {
-  return this._httpClient.PostData('OTBooking/OTBookingRequestEmergencyList', {
-    fromDate,
-    toDate
-  });
-}
+        return this._httpClient.PostData('OTBooking/OTBookingRequestEmergencyList', {
+            fromDate,
+            toDate
+        });
+    }
 
     initializeFormGroup() {
         this.createRequestForm();
     }
-populateForm(param) {
+    populateForm(param) {
         // this.personalFormGroup.patchValue(param);
     }
-   public getSurgeonsByDoctorType(doctTypeId) {
-        return this._httpClient.GetData("VisitDetail/DoctorTypeDoctorList?DocTypeId="+doctTypeId)
+    public getSurgeonsByDoctorType(doctTypeId) {
+        return this._httpClient.GetData("VisitDetail/DoctorTypeDoctorList?DocTypeId=" + doctTypeId)
     }
 
-     public OnCancel(param){
-    return this._httpClient.PostData('OTBooking/Cancel',param)
-  }
+    public OnCancel(param) {
+        return this._httpClient.PostData('OTBooking/Cancel', param)
+    }
     public requestSave(Param: any) {
         if (Param.otbookingId) {
             return this._httpClient.PutData("OTBooking/Edit/" + Param.otbookingId, Param);
