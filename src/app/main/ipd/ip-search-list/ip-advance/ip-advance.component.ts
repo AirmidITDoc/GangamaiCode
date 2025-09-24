02e745be2647c155bbe26142f9596adf6ee8f249
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2025-09-24 23:11:53 +0530

    Advance page added Unit Id

--- src/app/main/ipd/ip-search-list/ip-advance/ip-advance.component.ts
+++ src/app/main/ipd/ip-search-list/ip-advance/ip-advance.component.ts
@@ -157,7 +157,8 @@ export class IPAdvanceComponent implements OnInit {
         isCancelled: [false],
         isCancelledBy: [0, [this._FormvalidationserviceService.onlyNumberValidator()]],
         isCancelledDate: ['1900-01-01', [ this._FormvalidationserviceService.validDateValidator()]],
-        advanceId: [0, [this._FormvalidationserviceService.onlyNumberValidator()]]
+        advanceId: [0, [this._FormvalidationserviceService.onlyNumberValidator()]],
+        unitId: [1]
       }),
       // details 
       advanceDetail: this.formBuilder.group({
@@ -183,6 +184,7 @@ export class IPAdvanceComponent implements OnInit {
         isCancelledDate: ['1900-01-01', [ this._FormvalidationserviceService.validDateValidator()]],
         reason: [''],
         advanceDetailId: [0, [this._FormvalidationserviceService.onlyNumberValidator()]],
+        unitId: [1]
       }), 
       //advanceupdate header
       advanceupdate: this.formBuilder.group({ 
