183953000bc865553fa71d353247c806effc2a11
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2025-10-28 23:08:27 +0530

    admin

--- src/app/main/administration/administration.module.ts
+++ src/app/main/administration/administration.module.ts
@@ -39,12 +39,12 @@ const appRoutes: Routes = [
     path: "reportconfiguration",
     loadChildren: () => import("./report-configuration/report-configuration.module").then((m) => m.ReportConfigurationModule),
   },
-  //  {
-  //   path:"dischargecancel",
-  //   loadChildren: () => import("./discharge-cancel/discharge-cancel.module").then((m) => m.DischargeCancelModule),
-  //  },
-  {
+   {
     path:"dischargecancel",
+    loadChildren: () => import("./discharge-cancel/discharge-cancel.module").then((m) => m.DischargeCancelModule),
+   },
+  {
+    path:"dischargecancel1",
     loadChildren: () => import("./new-administrative-task/administrativetask.module").then((m) => m.AdministrativetaskModule),
    },
    {
