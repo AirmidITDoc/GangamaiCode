d1b81ba6b5809cf0f52c6707842c171c48f9b0e9
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2025-12-19 11:02:58 +0530

    editmail

--- src/app/main/administration/configuration/edit-emailconfig/edit-emailconfig.component.ts
+++ src/app/main/administration/configuration/edit-emailconfig/edit-emailconfig.component.ts
@@ -1,81 +1,132 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
 import { ConfigurationService } from '../configuration.service';
-import { MatDialog } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { AuthenticationService } from 'app/core/services/authentication.service';
 import { FormGroup, UntypedFormBuilder } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { DatePipe } from '@angular/common';
+import { fuseAnimations } from '@fuse/animations';
 
 @Component({
   selector: 'app-edit-emailconfig',
   templateUrl: './edit-emailconfig.component.html',
-  styleUrls: ['./edit-emailconfig.component.scss']
+  styleUrls: ['./edit-emailconfig.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations,
 })
 export class EditEmailconfigComponent {
 
   EmailForm: FormGroup
 
-   displayName:any;
-   emailAddress:any;
-   mailServerSmtp:any;
-   smtpPort:any;
-   serverTimeout:any;
-   smtpRequiredAuthentication:any;
-   requiredSquiredPasswordAuthentication:any;
-   userName:any;
-   password:any;
-   smtpSsl:any;
-
+   vdisplayName:any;
+   vemailAddress:any;
+   vmailServerSmtp:any;
+   vsmtpPort:any;
+   vserverTimeout:any;
+   vsmtpRequiredAuthentication:any;
+   vrequiredSquiredPasswordAuthentication:any;
+   vuserName:any;
+   vpassword:any;
+   vsmtpSsl:any;
+   dateTimeObj:any
+ screenFromString = 'Common-form';
   constructor(
     public _ConfigurationService: ConfigurationService,
     private _loggedService: AuthenticationService,
-    public datePipe: DatePipe,
+    public datePipe: DatePipe, @Inject(MAT_DIALOG_DATA) public data: any,
     public _matDialog: MatDialog, public _formbuilder: UntypedFormBuilder,
     public toastr: ToastrService,
   ) { }
 
+ 
   ngOnInit(): void {
 
     this.EmailForm = this.CreateEmailForm()
-
+    debugger
+    console.log(this.data)
+    this.vdisplayName=this.data.displayname
+   this.vemailAddress=this.data.emailaddress
+   this.vmailServerSmtp=this.data.mailserver
+   this.vsmtpPort=this.data.smtpPort
+   this.vserverTimeout=this.data.serverTimeout
+   this.vsmtpRequiredAuthentication=this.data.reqAuthenticate
+   this.vrequiredSquiredPasswordAuthentication=this.data.passauthenticate
+   this.vuserName=this.data.userName
+   this.vpassword=this.data.password
+   this.vsmtpSsl=this.data.smtpSsl
   }
 
 
   CreateEmailForm() {
     return this._formbuilder.group({
-      "id": 1,
-      "displayName": "ATISupport",
-      "emailAddress": "support@airmidtechinnovations.com",
-      "mailServerSmtp": "mail.airmidtechinnovations.com",
-      "smtpPort": 587,
-      "serverTimeout": 2000,
-      "smtpRequiredAuthentication": true,
-      "requiredSquiredPasswordAuthentication": true,
-      "userName": "support@airmidtechinnovations.com",
-      "password": "support@13#",
-      "isActive": true,
-      "smtpSsl": true
+      id:[''],
+      displayName:[''],
+      emailAddress:[''],
+      mailServerSmtp:[''],
+      smtpPort:[''],
+      serverTimeout:[''],
+      smtpRequiredAuthentication:['1'],
+      requiredSquiredPasswordAuthentication:['1'],
+      userName:[''],
+      password:[''],
+      isActive:['1'],
+      smtpSsl:[''],
     });
   }
 
   OnSave() {
-    // if ((this.vMessage == '' || this.vMessage == null || this.vMessage == undefined)) {
-    //   this.toastr.warning('Please enter message', 'Warning !', {
-    //     toastClass: 'tostr-tost custom-toast-warning',
-    //   });
-    //   return;
-    // }
-
+   
 
       console.log(this.EmailForm.value)
+      if(this.EmailForm.valid){
       this._ConfigurationService.Emailconfigedit(this.EmailForm.value).subscribe((response) => {
-        this.toastr.success(response.message);
+             }); 
+            }
+              else {
+      let invalidFields = [];
 
-      }, (error) => {
-        this.toastr.error(error.message);
-      }); 
-    
+      if (this.EmailForm.invalid) {
+        for (const controlName in this.EmailForm.controls) {
+          if (this.EmailForm.controls[controlName].invalid) {
+            invalidFields.push(`Email Config Edit Form: ${controlName}`);
+          }
+        }
+      }
+      if (invalidFields.length > 0) {
+        invalidFields.forEach(field => {
+          this.toastr.warning(`Field "${field}" is invalid.`, 'Warning',
+          );
+        });
+      }
+
+    }
   }
+
+
+  
+  getDateTime(dateTimeObj) {
+    this.dateTimeObj = dateTimeObj;
+  }
+
+   
+  getValidationMessages() {
+    return {
+      displayName: [
+        { name: "required", Message: "displayName is required" }
+      ],
+      emailAddress: [{ name: "required", Message: "emailAddress is required" }],
+      mailServerSmtp: [{ name: "required", Message: "mailServerSmtp is required" }],
+      smtpPort: [{ name: "required", Message: "smtpPort is required" }],
+      serverTimeout: [{ name: "required", Message: "serverTimeout is required" }],
+      userName: [{ name: "required", Message: "userName is required" }],
+      password: [{ name: "required", Message: "password is required" }],
+      smtpSsl: [{ name: "required", Message: "smtpSsl is required" }],
+      
+    };
+  }
+
+
+
   OnReset() {
     this.onClose();
   }
