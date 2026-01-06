479d6dfa4c6983a7eed542b5fe5f08930ba0639c
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2026-01-06 18:04:18 +0530

    Inpatient Browse list

---
+++ src/app/main/pharmacy/inpatientbrowse-list/inpatientbrowse-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inpatientbrowse-list',
+  templateUrl: './inpatientbrowse-list.component.html',
+  styleUrls: ['./inpatientbrowse-list.component.scss']
+})
+export class InpatientbrowseListComponent {
+
+}
