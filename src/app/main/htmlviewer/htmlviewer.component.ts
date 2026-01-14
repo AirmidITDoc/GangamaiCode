a07118ddabbf273394b4e2d9f890e385f3a1a9df
Vimal Vataliya <elaunch.vimal@gmail.com>
2026-01-14 19:46:38 +0530

    remove margin

--- src/app/main/htmlviewer/htmlviewer.component.ts
+++ src/app/main/htmlviewer/htmlviewer.component.ts
@@ -61,11 +61,6 @@ export class HtmlviewerComponent implements OnInit, AfterViewInit {
     <html>
       <head>
         <title>Print</title>
-        <style>
-          body { font-family: Arial; padding: 20px; }
-          table { border-collapse: collapse; width: 100%; }
-          th, td { border: 1px solid black; padding: 8px; }
-        </style>
       </head>
       <body>
         ${finalPrint}
