6b71b7c575b6568a58475583f5483ca193c5fb92
Vimal Vataliya <elaunch.vimal@gmail.com>
2026-01-14 19:48:15 +0530

    remove margin from html preview

--- src/app/main/htmlviewer/htmlviewer.component.ts
+++ src/app/main/htmlviewer/htmlviewer.component.ts
@@ -32,11 +32,7 @@ export class HtmlviewerComponent implements OnInit, AfterViewInit {
         doc.write(`
     <html>
       <head>
-        <style>
-          body { font-family: Arial; margin: 20px; }
-          table { border-collapse: collapse; width: 100%; }
-          th, td { border: 1px solid #000; padding: 6px; }
-        </style>
+        <title>Print</title>
       </head>
       <body>
         ${this.html}
