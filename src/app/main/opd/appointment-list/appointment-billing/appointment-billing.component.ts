c0dd690f5971b6d46c2d51d7f1a3da05748a2202
Ambadas <ambadasgajul1999@gmail.com>
2026-09-09 17:50:37 +0530

    op bill

--- src/app/main/opd/appointment-list/appointment-billing/appointment-billing.component.ts
+++ src/app/main/opd/appointment-list/appointment-billing/appointment-billing.component.ts
@@ -1077,6 +1077,7 @@ export class AppointmentBillingComponent implements OnInit, OnDestroy {
     }
     // Calculation of total amount.
     calculateTotalAmount(): void {
+        debugger
         const totalSum = this.chargeList.reduce((sum, charge) => sum + (+charge.TotalAmt), 0);
         const totalDiscount = this.chargeList.reduce((sum, charge) => sum + (+charge.DiscAmt), 0);
         const totalNet = totalSum - totalDiscount;
@@ -1097,13 +1098,15 @@ export class AppointmentBillingComponent implements OnInit, OnDestroy {
             this.Consessionres = true
         }
         else {
-            // this.OPFooterForm.patchValue({
-            //     totalDiscountPer: 0,
-            //     concessionAmt: 0
-            // }, { emitEvent: false });
-            this.OPFooterForm.get("concessionAmt").value == 0 ? this.isDiscountApplied = false : this.isDiscountApplied = true
-        }
-
+    const concessionAmt = Number(this.OPFooterForm.get('concessionAmt')?.value || 0 ); 
+    this.isDiscountApplied = concessionAmt > 0;           
+      if (!this.isDiscountApplied) {
+        this.OPFooterForm.patchValue({
+            totalDiscountPer: 0,
+            concessionAmt: 0
+        }, { emitEvent: false });
+    }
+        }   
     }
     onPriceOrQtyChange(row: ChargesList = null): void {
         if (!row) return;
