71755229916ce8f80a74e32ee4c0b90df61b485c
rakshanetalkar <163313832+rakshanetalkar@users.noreply.github.com>
2026-09-21 10:01:05 +0530

    constant service

--- src/app/main/setup/constant-masters/constant-masters.service.ts
+++ src/app/main/setup/constant-masters/constant-masters.service.ts
@@ -16,19 +16,24 @@ export class ConstantMastersService {
 
   Form(): FormGroup {
     return this._formBuilder.group({
-      constantId:[0, [this._FormvalidationserviceService.onlyNumberValidator()]],
+      constantId: [0, [this._FormvalidationserviceService.onlyNumberValidator()]],
       name: ['', [Validators.required]],
       value: ['', [Validators.required]],
+      constantType: ['', [Validators.required]],
     });
   }
 
   public constantMasterSave(Param: any) {
     if (Param.constantId) {
       return this._httpClient.PutData("Constants/" + Param.constantId, Param);
-    }
+    } else return this._httpClient.PostData("Constants", Param);
   }
 
-   public getconstantType() {
-        return this._httpClient.GetData("Constants/search-ConstantsType?Keyword=%");
-    }
+  public getconstantType() {
+    return this._httpClient.GetData("Constants/search-ConstantsType?Keyword=%");
+  }
+
+  public deactivateTheStatus(m_data) {
+    return this._httpClient.DeleteData("Constants?Id=" + m_data.toString());
+  }
 }
