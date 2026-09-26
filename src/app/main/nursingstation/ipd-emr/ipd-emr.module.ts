76daf0ca3a9b719044067f3becf065acbd82a1a7
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2026-09-26 10:49:12 +0530

    IP EMR code

--- src/app/main/nursingstation/ipd-emr/ipd-emr.module.ts
+++ src/app/main/nursingstation/ipd-emr/ipd-emr.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { IpdEMRComponent } from './ipd-emr.component';
 import { MatIconModule } from '@angular/material/icon';
-
-// const routes: Routes = [
-//     {
-//         path: "**",
-//         component: IpdEMRComponent,
-//     },
-// ];
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 const routes: Routes = [
     {
@@ -25,7 +25,13 @@ const routes: Routes = [
   imports: [
     RouterModule.forChild(routes),
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule,
+    MatCardModule,
+    MatTabsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: []
 })
